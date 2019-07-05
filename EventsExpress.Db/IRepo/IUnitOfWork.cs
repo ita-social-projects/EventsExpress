2eaacd892cd7c6748ce4ac92052d28d57241ea71
Dima Kundii <dima.kundiy14@gmail.com>
2019-07-05 17:40:27 +0300

    Add some changes in IUnitOfWork file

--- EventsExpress.Db/IRepo/IUnitOfWork.cs
+++ EventsExpress.Db/IRepo/IUnitOfWork.cs
@@ -8,6 +8,19 @@ namespace EventsExpress.Db.IRepo
 {
     public interface IUnitOfWork : IDisposable
     {
+         ICategoryRepository CategoryRepository { get; }
+         ICityRepository CityRepository { get; }
+         ICommentsRepository CommentsRepository { get; }
+         ICountryRepository CountryRepository { get; }
+         IEventRepository EventRepository { get; }
+         IPermissionRepository PermissionRepository { get; }
+         IPhotoRepository PhotoRepository { get; }
+         IRateRepository RateRepository { get; }
+         IRelationshipRepository RelationshipRepository { get; }
+         IReportRepository ReportRepository { get; }
+         IRoleRepository RoleRepository { get; }
+         IUserRepository UserRepository { get; }
+
         Task SaveAsync();
     }
 }
