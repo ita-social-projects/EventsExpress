8fae0eb06753530b8870318d74c8677d060273ce
Александр Савчук <savchuk.sand0@gmail.com>
2019-07-05 18:23:41 +0300

    Start to implement AUTH Service

---
+++ EventsExpress.Core/IServices/IAuthServicre.cs
@@ -0,0 +1,14 @@
+﻿using EventsExpress.Db.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsExpress.Core.IServices
+{
+    interface IAuthServicre
+    {
+        Task<bool> AuthenticateAsync(string name, string password);
+        Task SignOutAsync();
+    }
+}
---
+++ EventsExpress.Core/Services/AuthServicre.cs
@@ -0,0 +1,29 @@
+﻿using EventsExpress.Core.IServices;
+using EventsExpress.Db.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsExpress.Core.Services
+{
+    public class AuthServicre : IAuthServicre
+    {
+        // private readonly IUnitOfWork _uow
+        public AuthServicre()
+        {
+            //_uow = uow;
+        }
+
+
+        public Task<bool> AuthenticateAsync(string name, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SignOutAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
