dbcccc10c2c3a104957c806e17acfdf306d60439
Dima Kundii <dima.kundiy14@gmail.com>
2019-07-18 00:12:00 +0300

    Second version

--- EventsExpress/ClientApp/src/components/SelectCategories/SelectCategories.js
+++ EventsExpress/ClientApp/src/components/SelectCategories/SelectCategories.js
@@ -2,28 +2,24 @@
 import { Field, reduxForm } from 'redux-form'
 import Multiselect from 'react-widgets/lib/Multiselect'
 import Button from "@material-ui/core/Button";
-import clsx from 'clsx';
-import Select from 'react-select';
 import { emphasize, makeStyles, useTheme } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import NoSsr from '@material-ui/core/NoSsr';
-import TextField from '@material-ui/core/TextField';
-import Paper from '@material-ui/core/Paper';
-import Chip from '@material-ui/core/Chip';
-import MenuItem from '@material-ui/core/MenuItem';
-import CancelIcon from '@material-ui/icons/Cancel';
-import PropTypes from 'prop-types';
 const suggestions = [
-    { label: 'Summer' },
-    { label: 'Mount' },
-    { label: 'Party' },
-    { label: 'Gaming' },
-    { label: 'Golf' }
-].map(suggestion => ({
-    value: suggestion.label,
-    label: suggestion.label,
-}));
+  'Summer' ,
+ 'Mount' ,
+  'Party' ,
+ 'Gaming' ,
+ 'Golf' 
+];
 
+
+const renderMultiselect = ({ input, data, valueField, textField }) =>
+    <Multiselect {...input}
+        onBlur={() => input.onBlur()}
+        value={input.value || []} // requires value to be an array
+        data={data}
+        valueField={valueField}
+        textField={textField}
+    />
 const useStyles = makeStyles(theme => ({
     root: {
         flexGrow: 1,
@@ -74,188 +70,10 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 
-function NoOptionsMessage(props) {
-    return (
-        <Typography
-            color="textSecondary"
-            className={props.selectProps.classes.noOptionsMessage}
-            {...props.innerProps}
-        >
-            {props.children}
-        </Typography>
-    );
-}
-
-NoOptionsMessage.propTypes = {
-    children: PropTypes.node,
-    innerProps: PropTypes.object,
-    selectProps: PropTypes.object.isRequired,
-};
-
-function inputComponent({ inputRef, ...props }) {
-    return <div ref={inputRef} {...props} />;
-}
-
-inputComponent.propTypes = {
-    inputRef: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
-};
-
-function Control(props) {
-    const {
-        children,
-        innerProps,
-        innerRef,
-        selectProps: { classes, TextFieldProps },
-    } = props;
-
-    return (
-        <TextField
-            fullWidth
-            InputProps={{
-                inputComponent,
-                inputProps: {
-                    className: classes.input,
-                    ref: innerRef,
-                    children,
-                    ...innerProps,
-                },
-            }}
-            {...TextFieldProps}
-        />
-    );
-}
-
-Control.propTypes = {
-    children: PropTypes.node,
-    innerProps: PropTypes.object,
-    innerRef: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
-    selectProps: PropTypes.object.isRequired,
-};
-
-function Option(props) {
-    return (
-        <MenuItem
-            ref={props.innerRef}
-            selected={props.isFocused}
-            component="div"
-            style={{
-                fontWeight: props.isSelected ? 500 : 400,
-            }}
-            {...props.innerProps}
-        >
-            {props.children}
-        </MenuItem>
-    );
-}
-
-Option.propTypes = {
-    children: PropTypes.node,
-    innerProps: PropTypes.object,
-    innerRef: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
-    isFocused: PropTypes.bool,
-    isSelected: PropTypes.bool,
-};
-
-function Placeholder(props) {
-    return (
-        <Typography
-            color="textSecondary"
-            className={props.selectProps.classes.placeholder}
-            {...props.innerProps}
-        >
-            {props.children}
-        </Typography>
-    );
-}
-
-Placeholder.propTypes = {
-    children: PropTypes.node,
-    innerProps: PropTypes.object,
-    selectProps: PropTypes.object.isRequired,
-};
-
-function SingleValue(props) {
-    return (
-        <Typography className={props.selectProps.classes.singleValue} {...props.innerProps}>
-            {props.children}
-        </Typography>
-    );
-}
-
-SingleValue.propTypes = {
-    children: PropTypes.node,
-    innerProps: PropTypes.object,
-    selectProps: PropTypes.object.isRequired,
-};
-
-function ValueContainer(props) {
-    return <div className={props.selectProps.classes.valueContainer}>{props.children}</div>;
-}
-
-ValueContainer.propTypes = {
-    children: PropTypes.node,
-    selectProps: PropTypes.object.isRequired,
-};
-
-function MultiValue(props) {
-    return (
-        <Chip
-            tabIndex={-1}
-            label={props.children}
-            className={clsx(props.selectProps.classes.chip, {
-                [props.selectProps.classes.chipFocused]: props.isFocused,
-            })}
-            onDelete={props.removeProps.onClick}
-            deleteIcon={<CancelIcon {...props.removeProps} />}
-        />
-    );
-}
-
-MultiValue.propTypes = {
-    children: PropTypes.node,
-    isFocused: PropTypes.bool,
-    removeProps: PropTypes.object.isRequired,
-    selectProps: PropTypes.object.isRequired,
-};
-
-function Menu(props) {
-    return (
-        <Paper square className={props.selectProps.classes.paper} {...props.innerProps}>
-            {props.children}
-        </Paper>
-    );
-}
-
-Menu.propTypes = {
-    children: PropTypes.node,
-    innerProps: PropTypes.object,
-    selectProps: PropTypes.object,
-};
-
-const components = {
-    Control,
-    Menu,
-    MultiValue,
-    NoOptionsMessage,
-    Option,
-    Placeholder,
-    SingleValue,
-    ValueContainer,
-};
-
-
-
-
 function SelectCategories(props) {
-   
-    const { handleSubmit, pristine, submitting } = props
+    const { handleSubmit, submitting } = props
     const classes = useStyles();
     const theme = useTheme();
-    const [multi, setMulti] = React.useState(null);
-    function handleChangeMulti(value) {
-        setMulti(value);
-    }
-
     const selectStyles = {
         input: base => ({
             ...base,
@@ -265,43 +83,33 @@ function SelectCategories(props) {
             },
         }),
     };
-    const renderMultiselect = ({ input, data, valueField, textField }) =>
-        <Select
-            classes={classes}
-            styles={selectStyles}
-            inputId="react-select-multiple"
-            TextFieldProps={{
-                label: 'Categories',
-                InputLabelProps: {
-                    htmlFor: 'react-select-multiple',
-                    shrink: true,
-                },
-                placeholder: 'Select multiple countries',
-            }}
-            options={suggestions}
-            components={components}
-            value={multi}
-            onChange={handleChangeMulti}
-            isMulti
-        />
     return ( 
+        <div className={classes.root}>
+          
         <form onSubmit={handleSubmit}>
-                <div className={classes.root}>
-                    <NoSsr> 
-                        <div className={classes.divider} />
-                        <Field
+                <Field
+                    classes={classes}
+                    styles={selectStyles}
+                    inputId="react-select-single"
                             name="Categories"
                             component={renderMultiselect}
-                            data={suggestions}
+                    data={suggestions}
+                    TextFieldProps={{
+                        label: 'Country',
+                        InputLabelProps: {
+                            htmlFor: 'react-select-single',
+                            shrink: true,
+                        },
+                        placeholder: 'Search a country (start with a)',
+                    }}
                     />
                     <div>
                         <Button type="submit" color="primary" disabled={ submitting} >
                             Save
                         </Button >
-                    </div>
-                  </NoSsr>
             </div>
-        </form>  
+            </form>
+        </div>
   )
 } 
 export default reduxForm({
