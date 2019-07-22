a038c46d2c984dc928af717c514f42bd88c3f604
Misha Lytva <asonyalox@gmail.com>
2019-07-22 18:04:55 +0300

    Start work with user categories

---
+++ EventsExpress/ClientApp/src/actions/EditProfile/addUserCategory.js
@@ -0,0 +1,48 @@
+﻿import EventsExpressService from '../../services/EventsExpressService';
+
+export const addUserCategory = {
+    PENDING: "SET_ADDUSERCATEGORY_PENDING",
+    SUCCESS: "SET_ADDUSERCATEGORY_SUCCESS",
+    ERROR: "SET_ADDUSERCATEGORY_ERROR"
+}
+
+const api_serv = new EventsExpressService();
+
+export default function add_UserCategory(data) {
+    return dispatch => {
+        dispatch(setAddUserCategoryPending(true));
+        const res = api_serv.setUserCategories(data);
+        res.then(response => {
+            if (response.error == null) {
+
+                dispatch(setAddUserCategorySuccess(true));
+
+            } else {
+                dispatch(setAddUserCategorySuccess(response.error));
+            }
+        });
+
+    }
+}
+
+function setAddUserCategoryPending(data) {
+    return {
+        type: addUserCategory.PENDING,
+        payload: data
+    };
+}
+
+function setAddUserCategorySuccess(data) {
+    return {
+        type: addUserCategory.SUCCESS,
+        payload: data
+    };
+}
+
+function setAddUserCategoryError(data) {
+    return {
+        type: addUserCategory.ERROR,
+        payload: data
+    };
+}
+
---
+++ EventsExpress/ClientApp/src/containers/editProfileContainers/addUserCategoryContainer.js
@@ -0,0 +1,26 @@
+﻿import React from "react";
+import CategoryForm from '../components/category/category-form';
+import { connect } from "react-redux";
+import add from "../actions/add-category";
+
+class AddUserCategory extends React.Component {
+    submit = values => {
+        console.log(values);
+        add(values);
+    };
+
+    render() {
+        return <SelectCategoriesWrapper callback={this.submit} />;
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        add: (data) => dispatch(add_UserCategory(data))
+    };
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(AddUserCategory);
