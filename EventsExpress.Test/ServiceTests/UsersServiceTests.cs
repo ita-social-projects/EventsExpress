a6cc23691f4509e27988ff3c863b20c99ccb9398
Misha Lytva <asonyalox@gmail.com>
2019-08-09 17:44:53 +0300

    Unit tests start

---
+++ EventsExpress.Test/ServiceTests/UsersServiceTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EventsExpress.Core.Services;
+using Moq;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using AutoMapper;
+using EventsExpress.Core.IServices;
+using MediatR;
+using EventsExpress.Core.Infrastructure;
+using EventsExpress.Db.Entities;
+
+namespace EventsExpress.Test.ServiceTests
+{    [TestFixture]
+    class UsersServiceTests: TestInitializer
+    {
+        private UserService service;
+        private User user;
+
+        private static Mock<IPhotoService> mockPhotoService;
+        private static Mock<IMediator> mockMediator;
+        private static Mock<IEmailService> mockEmailService;
+        private static Mock<CacheHelper> mockCacheHelper;
+
+        [SetUp]
+        protected override void Initialize()
+        {
+            mockMediator = new Mock<IMediator>();
+            mockPhotoService = new Mock<IPhotoService>();
+            mockEmailService = new Mock<IEmailService>();
+            mockCacheHelper = new Mock<CacheHelper>();
+
+            service = new UserService(mockUnitOfWork.Object, mockMapper.Object, mockPhotoService.Object, mockMediator.Object, mockCacheHelper.Object, mockEmailService.Object);
+
+            user = new User() {Email="aaa@gmail.com", Id= '76ada198-0ad5-45d0-f173-08d704560843' };
+
+        }
+
+        [Test]
+        public void Create_EmeilExisting_returnFalse()
+        {
+            mockUnitOfWork.Setup(u=>u.UserRepository.Filter());
+        }
+    }
+
+}
---
+++ EventsExpress.Test/TestInitializer.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Moq;
+using EventsExpress.Db.IRepo;
+
+
+namespace EventsExpress.Test
+{
+    [TestFixture]
+    public abstract class TestInitializer
+    {
+        protected static Mock<IUnitOfWork> mockUnitOfWork;
+        protected static Mock<IMapper> mockMapper;
+       
+
+
+        [SetUp]
+        protected virtual void Initialize()
+        {
+            mockUnitOfWork = new Mock<IUnitOfWork>();
+            mockMapper = new Mock<IMapper>();
+            
+            TestContext.WriteLine("Initialize test data");
+        }
+
+        [TearDown]
+        protected virtual void Cleanup()
+        {
+            TestContext.WriteLine("Cleanup test data");
+        }
+
+    }
+}
