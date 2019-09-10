d3a491cc0bcdf45186ed550ac639cfcd51c953a3
Александр Савчук <savchuk.sand0@gmail.com>
2019-09-10 12:33:00 +0300

    improove presentation of user profile

--- EventsExpress/ClientApp/src/components/profile/user-profile.js
+++ EventsExpress/ClientApp/src/components/profile/user-profile.js
@@ -12,11 +12,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import PhoneIcon from '@material-ui/icons/Phone';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import PersonPinIcon from '@material-ui/icons/PersonPin';
-import HelpIcon from '@material-ui/icons/Help';
-import ShoppingBasket from '@material-ui/icons/ShoppingBasket';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import CustomAvatar from '../avatar/custom-avatar';
@@ -115,24 +110,20 @@ export default class UserItemView extends Component {
 
         const categories_list = this.renderCategories(categories);
 
-        return <>
-                <div className="row info">
 
-                    <div className="col-3">
-                        <h6><strong><p className="font-weight-bolder" key={name} >User Name:</p></strong></h6>
-                        <h6><strong><p className="font-weight-bolder" >Age:</p></strong></h6>
-                        <h6><strong><p className="font-weight-bolder" >Gender:</p></strong></h6>
-                        <h6><strong><p className="font-weight-bolder" >Email:</p></strong></h6>
-                        <h6><strong><p className="font-weight-bolder" >Interests:</p></strong></h6>
-                    </div>
-                    <div className="col-3">
-                        {(name) ? <h6><strong><p className="font-weight-bolder" >{name}</p></strong></h6> : <h6><strong><p className="font-weight-bolder" >---</p></strong></h6>}
-                        {(this.getAge(birthday)) ? <h6><strong><p className="font-weight-bolder" >{this.getAge(birthday)}</p></strong></h6> : <h6><strong><p className="font-weight-bolder" >---</p></strong></h6>}
-                        {(genders[gender]) ? <h6><strong><p className="font-weight-bolder" >{genders[gender]}</p></strong></h6> : <h6><strong><p className="font-weight-bolder" >---</p></strong></h6>}
-                        {(email) ? <h6><strong><p className="font-weight-bolder" >{email}</p></strong></h6> : <h6><strong><p className="font-weight-bolder" >---</p></strong></h6>}
-                        {(categories_list) ? <h6><strong><p className="font-weight-bolder" >{categories_list}</p></strong></h6> : <h6><strong><p className="font-weight-bolder" >---</p></strong></h6>}
-                    </div>
-                    {(id !== this.props.current_user) &&
+        const render_prop = (propName, value) => (
+            <div className='row mb-3 font-weight-bold'>
+                <div className='col-4'>{propName + ':'}</div>
+                <div className='col-8'>
+                    {value ? value : '' }
+                </div>
+            </div>
+        )
+
+        return <>
+                <div className="info">
+                    {(id !== this.props.current_user) 
+                        ?
                         <div className="col-4 user">
                             <div className='d-flex flex-column justify-content-center align-items-center'>
                                 <div className="user-profile-avatar">
@@ -164,30 +155,35 @@ export default class UserItemView extends Component {
                                 </div>
                             </div>
                         </div>
-                    }
+                        :<div className="col-4"></div>
+                    }   
 
-                    {(id === this.props.current_user) &&
-                        <div className="col-2">
-                        </div>
-                    }
+                    <div className='col-sm-12  col-md-6'>
+                        {render_prop('User Name', name )}
+                        {render_prop('Age', this.getAge(birthday) )}
+                        {render_prop('Gender', genders[gender] )}
+                        {render_prop('Email', email )}
+                        {render_prop('Interests', categories_list )}
+                    </div>
                 </div>
+
                 <div className='mt-2'>
                     <AppBar position="static" color="inherit">
                         <Tabs
+                            className='w-100'
                             value={this.state.value}
                             onChange={this.handleChange}
                             variant="fullWidth"
                             scrollButtons="on"
                             indicatorColor="primary"
                             textColor="primary"
-                            aria-label="scrollable force tabs example"
                         >
-                            <Tab label="Future events" icon={<PhoneIcon />} {...a11yProps(0)} />
-                            <Tab label="Archive events" icon={<FavoriteIcon />} {...a11yProps(1)} />
-                            <Tab label="Visited events" icon={<PersonPinIcon />} {...a11yProps(2)} />
-                            <Tab label="Events to go" icon={<HelpIcon />} {...a11yProps(3)} />
+                            <Tab label="Future events" icon={<IconButton color={ this.state.value===0 ? '' : 'primary'}><i class="far fa-calendar-alt"></i></IconButton>} {...a11yProps(0)} />
+                            <Tab label="Archive events" icon={<IconButton color={ this.state.value===1 ? '' : 'primary'}><i class="fas fa-archive"></i></IconButton>} {...a11yProps(1)} />
+                            <Tab label="Visited events" icon={<IconButton color={ this.state.value===2 ? '' : 'primary'}><i class="fas fa-history"></i></IconButton>} {...a11yProps(2)} />
+                            <Tab label="Events to go" icon={<IconButton color={ this.state.value===3 ? '' : 'primary'}><i class="fas fa-map-marker-alt"></i></IconButton>} {...a11yProps(3)} />
                             {(id === this.props.current_user) &&
-                                <Tab label="Add event" icon={<ShoppingBasket />} {...a11yProps(4)} />
+                                <Tab label="Add event" icon={<IconButton color={ this.state.value===4 ? '' : 'primary'}><i class="fas fa-plus"></i></IconButton>} {...a11yProps(4)} />
                             }
                         </Tabs>
                     </AppBar>
