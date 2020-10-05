29af4082a6126fe379d959ff2bc3f766fd401113
Andrii712 <andrew.zaharovskyi@gmail.com>
2020-10-05 14:57:39 +0300

    Squashed commit of the following:
    commit a748f93b483e0f739c4a4e974be3fa7c9bdec6e8
    Merge: bf7ad2f 6d92079
    Author: Andrii Zaharovskyi <andrew.zaharovskyi@gmail.com>
    Date:   Wed Sep 30 17:13:43 2020 +0300
    
        Merge pull request #105 from EventsExpress-SoftServe/sand0-patch-1
    
        Delete EventsExpress.xml
    
    commit 6d92079c2ef419a4a81d453bf48ab0f42502a82a
    Author: Alexandr Savchuk <44262349+sand0@users.noreply.github.com>
    Date:   Wed Sep 30 17:08:24 2020 +0300
    
        Delete EventsExpress.xml
    
    commit bf7ad2f4a2173022e87d69137ff05325a77b257e
    Merge: 9a5beac da74169
    Author: Yevhen Vasylynchuk <45979893+Avalon412@users.noreply.github.com>
    Date:   Wed Sep 30 14:46:53 2020 +0300
    
        Merge pull request #104 from EventsExpress-SoftServe/99-fix-CommunaTimeAccoutning
    
        Fixed time accounting in 'Comunna'
    
    commit da7416931aa28fe917ede872af447c428834ca81
    Merge: a4f5073 9a5beac
    Author: Yevhen Vasylynchuk <45979893+Avalon412@users.noreply.github.com>
    Date:   Tue Sep 29 18:43:58 2020 +0300
    
        Merge branch 'development' into 99-fix-CommunaTimeAccoutning
    
    commit a4f50734bca8a087fddf29057d23b397181f5c3e
    Author: Женя Василинчук <bensonatg412@gmail.com>
    Date:   Tue Sep 29 18:31:54 2020 +0300
    
        Fixed time accounting in 'Comunna'

--- EventsExpress/ClientApp/src/components/chat/msg.js
+++ EventsExpress/ClientApp/src/components/chat/msg.js
@@ -5,6 +5,7 @@ import ButtonBase from '@material-ui/core/ButtonBase';
 import Avatar from '@material-ui/core/Avatar';
 import { deleteSeenMsgNotification } from '../../actions/chat';
 import './msg.css';
+import * as moment from 'moment';
 
 class Msg extends Component {
     componentDidUpdate = () => {
@@ -14,25 +15,8 @@ class Msg extends Component {
         }
     }
 
-    getTime = (value) => {
-        let today = new Date();
-        let times = new Date(value);
-        var time = today - times;
-        var years = Math.floor(time / 52 / 7 / 24 / 60 / 60 / 1000);
-        if (years != 0) return `${years} years ago`;
-        time -= years * 52 * 7 * 24 * 60 * 60 * 1000;
-        var weeks = Math.floor(time / 7 / 24 / 60 / 60 / 1000);
-        if (weeks != 0) return `${weeks} weeks ago`;
-        time -= weeks * 7 * 24 * 60 * 60 * 1000;
-        var days = Math.floor(time / 24 / 60 / 60 / 1000);
-        if (days != 0) return `${days} days ago`;
-        time -= days * 24 * 60 * 60 * 1000;
-        var hours = Math.floor(time / 60 / 60 / 1000);
-        if (hours != 0) return `${hours} hours ago`;
-        time -= hours * 60 * 60 * 1000;
-        var minutes = Math.floor(time / 60 / 1000);
-        if (minutes != 0) return `${minutes} minutes ago`;
-        return `right now`;
+    getTime = (value) => {        
+        return moment.utc(value).fromNow();
     }
 
     render() {
