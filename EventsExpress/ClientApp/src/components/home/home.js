befe2e43718db7d3d68be48f94abbcc18175dec8
Andrii712 <andrew.zaharovskyi@gmail.com>
2020-11-03 01:29:44 +0200

    Code has been refactored
    - EventListWrapper container call has been modified;
    - Outdated import of DialogContainer has been removed;

--- EventsExpress/ClientApp/src/components/home/home.js
+++ EventsExpress/ClientApp/src/components/home/home.js
@@ -1,28 +1,15 @@
 import React, { Component } from 'react';
-import './home.css';
 import EventListWrapper from '../../containers/event-list';
-import { connect } from 'react-redux';
 import EventFilterWrapper from '../../containers/event-filter';
+import './home.css';
 
 export default class Home extends Component {
     render() {
         return (<>
             <EventFilterWrapper />
             <div className="events-container">
-                {/* TODO: Remove comment after debug. */}
                 <EventListWrapper location={this.props.location} />
-                {/* <EventListWrapper params={this.props.searchParams} /> */}
-                {/* <EventListWrapper /> */}
             </div>
         </>);
     }
 }
-
-// const mapStateToProps = state => {
-//     return {
-//         id: state.user.id,
-//         searchParams: state.events.searchParams,
-//     };
-// };
-
-// export default connect(mapStateToProps)(Home);
