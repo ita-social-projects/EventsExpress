786b4a0e0375a8715403cbd1e09b626142cb82bc
Andrii712 <andrew.zaharovskyi@gmail.com>
2020-11-03 12:57:19 +0200

    Fixed Link import after merging

--- EventsExpress/ClientApp/src/components/comment/comment-item.js
+++ EventsExpress/ClientApp/src/components/comment/comment-item.js
@@ -1,4 +1,5 @@
 ﻿import React, { Component } from "react";
+import { Link } from 'react-router-dom';
 import Avatar from '@material-ui/core/Avatar';
 import './Comment.css';
 import { getTimeDifferenceFromNull } from '../helpers/TimeHelper';
@@ -12,35 +13,35 @@ export default class commentItem extends Component {
             <div>
                 <div>
                     <div className="row">
-                        {!(user === userId) && 
-							<div className="photo-container">
-                            	<Avatar
-                                	alt="Тут аватар"
-                                	src={userPhoto}
-                            	/>
-                            	<h1 className="text-secondary comment-text"> {getTimeDifferenceFromNull(date)}</h1>
-                        	</div>
-						}
+                        {!(user === userId) &&
+                            <div className="photo-container">
+                                <Avatar
+                                    alt="Тут аватар"
+                                    src={userPhoto}
+                                />
+                                <h1 className="text-secondary comment-text"> {getTimeDifferenceFromNull(date)}</h1>
+                            </div>
+                        }
                         <div className="mybutton">
                             <p>
                                 <Link to={'/user/' + userId} className="btn-custom">
-									<a className="float-left" href={`/user/${userId}`}>
-										<strong className="text-primary">{userName}</strong>
-									</a>
-								</Link>
+                                    <a className="float-left" href={`/user/${userId}`}>
+                                        <strong className="text-primary">{userName}</strong>
+                                    </a>
+                                </Link>
                             </p>
                             <div className="clearfix"></div>
                             <p>{text}</p>
                         </div>
-                        {(user === userId) && 
-							<div className="photo-container">
-                            	<Avatar
-                                	alt="Avatar"
-                                	src={userPhoto}
-                            	/>
-                            	<h1 className="text-secondary comment-text"> {getTimeDifferenceFromNull(date)}</h1>
-                        	</div>
-						}
+                        {(user === userId) &&
+                            <div className="photo-container">
+                                <Avatar
+                                    alt="Avatar"
+                                    src={userPhoto}
+                                />
+                                <h1 className="text-secondary comment-text"> {getTimeDifferenceFromNull(date)}</h1>
+                            </div>
+                        }
                     </div>
                 </div>
             </div>
