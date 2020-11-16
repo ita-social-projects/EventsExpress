1df351bfaeddc84521fbf1d52495e9229c51350d
Anastasiagumenuk9 <44962434+Anastasiagumenuk9@users.noreply.github.com>
2020-11-16 20:20:58 +0200

    Create OccurenceEventAutoMapper

---
+++ EventsExpress/Mapping/OccurenceEventAutoMapper.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Linq;
+using AutoMapper;
+using EventsExpress.Core.DTOs;
+using EventsExpress.Core.Extensions;
+using EventsExpress.Db.Entities;
+using EventsExpress.DTO;
+
+namespace EventsExpress.Mapping
+{
+    public class OccurenceEventAutoMapper : Profile
+    {
+        public OccurenceEventAutoMapper()
+        {
+            CreateMap<OccurenceEvent, OccurenceEventDTO>()
+           .ForMember(dest => dest.Id, opts => opts.MapFrom(src => src.Id))
+           .ForMember(dest => dest.Frequency, opts => opts.MapFrom(src => src.Frequency))
+           .ForMember(dest => dest.Periodicity, opts => opts.MapFrom(src => src.Periodicity))
+           .ForMember(dest => dest.LastRun, opts => opts.MapFrom(src => src.LastRun))
+           .ForMember(dest => dest.NextRun, opts => opts.MapFrom(src => src.NextRun))
+           .ForMember(dest => dest.IsActive, opts => opts.MapFrom(src => src.IsActive))
+           .ForMember(dest => dest.Event, opts => opts.MapFrom(src => new EventDTO
+           {
+               Id = src.Event.Id,
+               Title = src.Event.Title,
+               IsBlocked = src.Event.IsBlocked,
+               IsReccurent = true,
+               Description = src.Event.Description,
+               DateTo = src.Event.DateTo,
+               DateFrom = src.Event.DateFrom,
+               MaxParticipants = src.Event.MaxParticipants,
+               Frequency = src.Frequency,
+               Periodicity = src.Periodicity,
+               PhotoBytes = src.Event.Photo,
+               CityName = src.Event.City.Name,
+               CountryName = src.Event.City.Country.Name,
+               Categories = src.Event.Categories.Select(x => new CategoryDTO
+               {
+                   Id = x.Category.Id,
+                   Name = x.Category.Name
+               }),
+           }));
+
+            CreateMap<OccurenceEventDTO, OccurenceEvent>().ReverseMap();
+
+            CreateMap<OccurenceEventDTO, OccurenceEventDto>()
+                .ForMember(dest => dest.Event, opts => opts.MapFrom(src => new EventDTO
+                {
+                    Id = src.Event.Id,
+                    Title = src.Event.Title,
+                    IsBlocked = src.Event.IsBlocked,
+                    IsReccurent = true,
+                    Description = src.Event.Description,
+                    DateTo = src.Event.DateTo,
+                    DateFrom = src.Event.DateFrom,
+                    MaxParticipants = src.Event.MaxParticipants,
+                    Frequency = src.Frequency,
+                    Periodicity = src.Periodicity,
+                    PhotoUrl = src.Event.PhotoBytes.Img.ToRenderablePictureString(),
+                    CityName = src.Event.City.Name,
+                    CountryName = src.Event.City.Country.Name,
+                    Categories = src.Event.Categories,
+                }));
+
+            CreateMap<OccurenceEventDto, OccurenceEventDTO>()
+                .ForMember(dest => dest.Event, opts => opts.MapFrom(src => new EventDTO
+                {
+                    Id = src.Event.Id,
+                    Title = src.Event.Title,
+                    Description = src.Event.Description,
+                    DateTo = src.Event.DateTo,
+                    DateFrom = src.Event.DateFrom,
+                    MaxParticipants = src.Event.MaxParticipants,
+                    Frequency = src.Frequency,
+                    Periodicity = src.Periodicity,
+                    CityId = src.Event.CityId,
+                    IsReccurent = true,
+                    PhotoUrl = src.Event.PhotoBytes.Img.ToRenderablePictureString(),
+                    CityName = src.Event.City.Name,
+                    CountryName = src.Event.City.Country.Name,
+                }));
+
+            CreateMap<EventDTO, OccurenceEventDTO>()
+                .ForMember(dest => dest.EventId, opts => opts.MapFrom(src => src.Id))
+                .ForMember(dest => dest.LastRun, opts => opts.MapFrom(src => src.DateTo))
+                .ForMember(dest => dest.NextRun, opts => opts.MapFrom(src => DateTimeExtensions.AddDateUnit(src.Periodicity, src.Frequency, src.DateTo)))
+                .ForMember(dest => dest.CreatedBy, opts => opts.MapFrom(src => src.OwnerId))
+                .ForMember(dest => dest.CreatedDate, opts => opts.MapFrom(src => DateTime.Now))
+                .ForMember(dest => dest.ModifiedBy, opts => opts.MapFrom(src => src.OwnerId))
+                .ForMember(dest => dest.ModifiedDate, opts => opts.MapFrom(src => DateTime.Now))
+                .ForMember(dest => dest.Id, opts => opts.Ignore())
+                .ForMember(dest => dest.IsActive, opts => opts.MapFrom(src => true));
+        }
+    }
+}
