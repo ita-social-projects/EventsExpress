010220e821f7a6057d2527a77b11358a6367514f
Oleksandr Savchuk <savchuk.sand0@gmail.com>
2020-11-17 19:01:46 +0200

    test for failure

--- EventsExpress.Core/NotificationHandlers/BlockedEventHandler.cs
+++ EventsExpress.Core/NotificationHandlers/BlockedEventHandler.cs
@@ -46,7 +46,7 @@ Your event was blocked for some reason.
 To unblock it, edit this event, please: <a href='{eventLink}'>{even.Title}</>",
                 });
             }
-            catch (Exception ex)
+            catch (Ex
             {
                 _logger.LogError(ex.Message);
             }
