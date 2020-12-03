be672c12d486b9f48bc3b63403eba0c4796c25eb
Anastasiagumenuk9 <44962434+Anastasiagumenuk9@users.noreply.github.com>
2020-12-03 22:44:06 +0200

    Delete condition

--- EventsExpress/ClientApp/src/services/EventScheduleService.js
+++ EventsExpress/ClientApp/src/services/EventScheduleService.js
@@ -16,10 +16,8 @@ export default class EventScheduleService {
 
     setEventSchedule = async (data) => {
         let file = new FormData();
-        if (data.id != null) {
-            file.append('Id', data.id);
-        }
 
+        file.append('Id', data.id);
         file.append('Frequency', data.frequency);
         file.append('LastRun', data.lastRun);
         file.append('NextRun', data.nextRun);
