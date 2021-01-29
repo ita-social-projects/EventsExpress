d86707e69ec4582bc015be8c5034029a00df0ecd
Denys Skibinskyi <ruslanskibinskii@gmail.com>
2021-01-29 15:15:36 +0200

    fixed submitting on event update

--- EventsExpress/ClientApp/src/actions/add-event.js
+++ EventsExpress/ClientApp/src/actions/add-event.js
@@ -12,54 +12,53 @@ const api_serv = new EventService();
 
 export default function add_event(data) {
 
-    return dispatch => {
-      dispatch(setEventPending(true));
-  
-      
-        return api_serv.setEvent(data).then(response => {
-        if(response.error == null){
-            dispatch(setEventSuccess(true));
-            return response.text().then(x => {
-                dispatch(eventWasCreated(x));
-                return Promise.resolve('success');
-            });
-        } else {
-            throw new SubmissionError(buildValidationState(response.error));
-          }
-        });
-    }
-}
-
-  
-export function edit_event(data) {
   return dispatch => {
     dispatch(setEventPending(true));
 
-    const res = api_serv.editEvent(data);
-    res.then(response => {
-      if(response.error == null){
+    return api_serv.setEvent(data).then(response => {
+      if (response.error == null) {
         dispatch(setEventSuccess(true));
-        dispatch(get_event(data.id));
-      }else{
-        dispatch(setEventError(response.error));
+
+        return response.text().then(x => {
+          dispatch(eventWasCreated(x));
+          return Promise.resolve('success');
+        });
+      } else {
+        throw new SubmissionError(buildValidationState(response.error));
       }
     });
   }
 }
 
-function eventWasCreated(eventId){
-  return{
+export function edit_event(data) {
+  return dispatch => {
+    dispatch(setEventPending(true));
+
+    return api_serv.editEvent(data).then(response => {
+      if (response.error == null) {
+        dispatch(setEventSuccess(true));
+        dispatch(get_event(data.id));
+        return Promise.resolve('success');
+      } else {
+        throw new SubmissionError(buildValidationState(response.error));
+      }
+    });
+  }
+}
+
+function eventWasCreated(eventId) {
+  return {
     type: EVENT_WAS_CREATED,
     payload: eventId
   }
 }
 
-  export function setEventSuccess(data) {
-    return {
-      type: SET_EVENT_SUCCESS,
-      payload: data
-    };
-  }
+export function setEventSuccess(data) {
+  return {
+    type: SET_EVENT_SUCCESS,
+    payload: data
+  };
+}
 
 export function setEventPending(data) {
   return {
