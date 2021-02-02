8c896194fa5d966b53aee41c6c9aa24e6d17b435
NastiaGerasymchuk <herasymchuk.anastasiia@chnu.edu.ua>
2021-02-02 18:45:43 +0200

    Corrected code smell

--- EventsExpress/ClientApp/src/components/event/map/display-online.js
+++ EventsExpress/ClientApp/src/components/event/map/display-online.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 class DisplayOnline extends Component {
     constructor(props) {
         super(props);
-    };
+    }
     render() {
         return (
             <div><a
