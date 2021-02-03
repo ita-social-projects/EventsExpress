7df3195df5bd84545fcde9f75380704e24924a1d
Denys Skibinskyi <39212854+bigzerooo@users.noreply.github.com>
2021-02-03 15:36:03 +0200

    #388 added categories validation (#397)
    * added categories validation
    
    * reworked Event image validation
    
    * added index check for existing
    
    * added BaseEventViewModelValidator Category tests
    
    * added EventEditViewModelValidator tests
    
    * added EventCreateViewModelValidator tests
    
    * replaced Guid

--- EventsExpress.Test/ValidatorTests/EventEditViewModelValidatorTests.cs
+++ EventsExpress.Test/ValidatorTests/EventEditViewModelValidatorTests.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using EventsExpress.Core.IServices;
 using EventsExpress.Db.Enums;
-using EventsExpress.Validation.Base;
 using EventsExpress.ViewModels;
-using EventsExpress.ViewModels.Base;
 using FluentValidation.TestHelper;
 using Moq;
 using NUnit.Framework;
@@ -11,18 +9,18 @@ using NUnit.Framework;
 namespace EventsExpress.Test.ValidatorTests
 {
     [TestFixture]
-    internal class BaseEventViewModelValidatorTests
+    internal class EventEditViewModelValidatorTests
     {
-        private BaseEventViewModelValidator<EventViewModelBase> validator;
-        private EventViewModelBase eventViewModel;
+        private EventEditViewModelValidator validator;
+        private EventEditViewModel eventViewModel;
         private Mock<ICategoryService> mockCategoryService;
 
         [SetUp]
         public void Setup()
         {
             mockCategoryService = new Mock<ICategoryService>();
-            validator = new BaseEventViewModelValidator<EventViewModelBase>(mockCategoryService.Object);
-            eventViewModel = new EventViewModelBase
+            validator = new EventEditViewModelValidator(mockCategoryService.Object);
+            eventViewModel = new EventEditViewModel
             {
                 Title = "Some title",
                 Description = "Some desc",
