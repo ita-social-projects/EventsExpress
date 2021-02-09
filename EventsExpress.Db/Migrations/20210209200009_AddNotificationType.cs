77209e0d3a6f5268caff5c5fd9736bf9c2e8790d
NastiaGerasymchuk <herasymchuk.anastasiia@chnu.edu.ua>
2021-02-09 23:44:24 +0200

    Added migration NotificationType

---
+++ EventsExpress.Db/Migrations/20210209200009_AddNotificationType.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EventsExpress.Db.Migrations
+{
+    public partial class AddNotificationType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NotificationTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NotificationTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserNotificationTypes",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(nullable: false),
+                    NotificationTypeId = table.Column<int>(nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserNotificationTypes", x => new { x.UserId, x.NotificationTypeId });
+                    table.ForeignKey(
+                        name: "FK_UserNotificationTypes_NotificationTypes_NotificationTypeId",
+                        column: x => x.NotificationTypeId,
+                        principalTable: "NotificationTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserNotificationTypes_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "NotificationTypes",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 1, "Own Event Change" });
+
+            migrationBuilder.InsertData(
+                table: "NotificationTypes",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 0, "Profile Change" });
+
+            migrationBuilder.InsertData(
+                table: "NotificationTypes",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 2, "Visited Event Change" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserNotificationTypes_NotificationTypeId",
+                table: "UserNotificationTypes",
+                column: "NotificationTypeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserNotificationTypes");
+
+            migrationBuilder.DropTable(
+                name: "NotificationTypes");
+        }
+    }
+}
