b2dc5c3a6d21ce32b28ec5505770f0f1b6b768ce
farmgun <sana23chel@gmail.com>
2021-02-26 14:38:47 +0200

    1

--- EventsExpress.Test/ControllerTests/EventControllerTests.cs
+++ EventsExpress.Test/ControllerTests/EventControllerTests.cs
@@ -44,12 +44,5 @@ namespace EventsExpress.Test.ControllerTests
             var expected = eventController.Create();
             Assert.IsInstanceOf<OkObjectResult>(expected);
         }
-
-        [Test]
-        public void AllDraft_OkResult()
-        {
-            var expected = eventController.AllDraft();
-            Assert.IsInstanceOf<OkObjectResult>(expected);
-        }
     }
 }
