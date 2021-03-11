da6588e28618b92aa004033871a58d2cf91a88f7
Oleksandr Slobodian <55383070+carendoh@users.noreply.github.com>
2021-03-11 16:42:15 +0300

    The entity configuration is divided into separate files. (#449)

---
+++ EventsExpress.Db/Configurations/EventCategoryConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using EventsExpress.Db.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EventsExpress.Db.Configuration
+{
+    public class EventCategoryConfiguration : IEntityTypeConfiguration<EventCategory>
+    {
+        public void Configure(EntityTypeBuilder<EventCategory> builder)
+        {
+            builder.HasKey(t => new { t.EventId, t.CategoryId });
+            builder.HasOne(ec => ec.Event)
+                .WithMany(e => e.Categories)
+                .HasForeignKey(ec => ec.EventId);
+            builder.HasOne(ec => ec.Category)
+                .WithMany(c => c.Events)
+                .HasForeignKey(uc => uc.CategoryId);
+        }
+    }
+}
