fcdede08e525f701f8bdff818c80774c0afdeccf
MaksymNykyforak <m.nyk@ukr.net>
2021-03-13 12:49:16 +0200

    Replaced 3 last elements from left-sidebar with Admin element.

--- EventsExpress/ClientApp/src/components/admin/admin.js
+++ EventsExpress/ClientApp/src/components/admin/admin.js
@@ -1,16 +1,52 @@
 import React, { Component } from 'react';
+import { Route } from "react-router-dom";
 import './admin.css';
+import { NavItem } from '../NavItem/NavItem';
+import Category from '../category/categories';
+import UserPWrapper from '../../containers/UsersWrapper';
+import UnitOfMeasuring from '../unitOfMeasuring/unitsOfMeasuring';
 
+export default class Admin extends Component {
 
-export default class Admin extends Component{
-    
-    render(){
-     
-    return(
-        <div>
-        
-        </div>
-        
-    );
+    render() {
+
+        return (
+            <>
+                <div className="wrapper-content">
+                    <div className="extra-sidebar">
+                        <ul className="list-unstyled">
+                            <nav id="sub-nav">
+                                <div className="item">
+                                    <NavItem
+                                        to={'/admin/categories/'}
+                                        icon={'fa fa-hashtag'}
+                                        text={"Categories"}
+                                    />
+                                </div>
+                                <div className="item">
+                                    <NavItem
+                                        to={'/admin/unitsOfMeasuring/'}
+                                        icon={'fa fa-plus'}
+                                        text={"UnitsOfMeasuring"}
+                                    />
+                                </div>
+                                <div className="item">
+                                    <NavItem
+                                        to={'/admin/users?page=1'}
+                                        icon={'fa fa-users'}
+                                        text={"Users"}
+                                    />
+                                </div>
+                            </nav>
+                        </ul>
+                    </div>
+                    <main>
+                        <Route path="/admin/categories/" component={Category} />
+                        <Route path='/admin/unitsOfMeasuring' component={UnitOfMeasuring} />
+                        <Route path="/admin/users" component={UserPWrapper} />
+                    </main>
+                </div>
+            </>
+        );
     }
 }
--- EventsExpress/ClientApp/src/components/app/app.js
+++ EventsExpress/ClientApp/src/components/app/app.js
@@ -7,9 +7,6 @@ import {
 } from 'react-router-dom';
 import Home from '../home';
 import Profile from '../profile';
-import Category from '../category/categories';
-import UsersWrapper from '../../containers/users';
-import UserPWrapper from '../../containers/UsersWrapper';
 import UserItemViewWrapper from '../../containers/user-item-view';
 import EventItemViewWrapper from '../../containers/event-item-view';
 import EventScheduleViewWrapper from '../../containers/event-Schedule-item-view';
@@ -17,15 +14,14 @@ import EventSchedulesListWrapper from '../../containers/eventSchedules-list';
 import Layout from '../layout';
 import SearchUserWrapper from '../../containers/UserSearchWrapper';
 import NotFound from '../Route guard/404';
-import EventsForAdmin from '../../components/event/EventsForAdmin';
 import Authentication from '../Authentication/authentication';
 import Chat from '../chat';
 import UserChats from '../chat/user_chats';
 import NotificationEvents from '../notification_events';
 import ContactUsWrapper from '../../containers/contactUs';
 import LoginTwitter from '../../containers/TwitterLogin';
-import UnitOfMeasuring from '../unitOfMeasuring/unitsOfMeasuring';
 import AddEventWrapper from '../../containers/add-event';
+import Admin from '../admin';
 
 export default class App extends Component {
     render() {
@@ -53,20 +49,16 @@ export default class App extends Component {
                         <Route path="/eventSchedules" component={EventSchedulesListWrapper} />
                         <Route path="/eventSchedule/:id" component={EventScheduleViewWrapper} />
                         <Route path="/user/:id" component={UserItemViewWrapper} />
-                        <Route path="/admin/categories/" component={Category} />
-                        <Route path="/admin/users" component={UserPWrapper} />
+                        <Route path="/admin" component={Admin} />                        
                         <Route path="/search/users" component={SearchUserWrapper} />
-                        <Route path="/admin/users" component={UsersWrapper} />
                         <Route path="/user_chats" component={UserChats} />
                         <Route path="/notification_events" component={NotificationEvents} />
                         <Route path="/authentication/:id/:token" component={Authentication} />
                         <Route path="/Authentication/TwitterLogin" component={LoginTwitter} />
                         <Route path="/chat/:chatId" component={Chat} />
-                        <Route path="/contactUs" component={ContactUsWrapper} />
-                        <Route path='/admin/unitsOfMeasuring' component={UnitOfMeasuring} />
+                        <Route path="/contactUs" component={ContactUsWrapper} />                        
                         <Route path='/event/createEvent' component={AddEventWrapper} />
-                        
-
+                                               
                         <Route component={NotFound} />
                     </Switch>
                 </Layout>
@@ -74,3 +66,4 @@ export default class App extends Component {
         );
     }
 }
+
