d095bdaf639532c68d53a76b110b418bdbd77efe
DennY <45600211+DennYats@users.noreply.github.com>
2021-04-02 12:12:46 +0300

    Divide events routes (#467)
    * Created components for different groups of events
    
    * Fix db connection
    
    * Added possibility to switch tabs by url
    
    * Event routes completed. Code refactored.
    
    * Events loading while page do. Styles fixed.
    
    * Bug with tabs fixed.
    
    * Selected tab showing after component updated from outside.
    
    * Unused imports removed.
    
    * Required changes are done.

---
+++ EventsExpress/ClientApp/src/components/profile/events.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import EventsForProfile from '../event/events-for-profile';
+import Spinner from '../spinner';
+import 'moment-timezone';
+import './User-profile.css';
+
+export default class Events extends Component {
+    render() {
+        const { isPending, data } = this.props.events;
+        const spinner = isPending ? <Spinner /> : null;
+        const content =  <EventsForProfile
+                data_list={data.items}
+                page={data.pageViewModel.pageNumber}
+                totalPages={data.pageViewModel.totalPages}
+                current_user={this.props.current_user}
+                callback={this.props.typeOfEvents} />;
+
+        return (
+            <div className="shadow pl-2 pr-2 pb-2 mb-5 bg-white rounded">
+                {spinner}{content}
+                {!isPending && !(data.items && data.items.length > 0) &&
+                    <div id="notfound" className="w-100">
+                        <div className="notfound">
+                            <div className="notfound-404">
+                                <div className="h1">No Results</div>
+                            </div>
+                        </div>
+                    </div>}
+            </div>
+        );
+    }
+}
