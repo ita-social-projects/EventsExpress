0ce2e46e624a4375d419eecd435f8518abf5b41a
DennY <45600211+DennYats@users.noreply.github.com>
2021-04-07 13:12:29 +0300

    Added auth policices by roles

---
+++ EventsExpress/Policies/RoleHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace EventsExpress.Policies
+{
+    public class RoleHandler : AuthorizationHandler<RoleRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, RoleRequirement requirement)
+        {
+            if (context.User.IsInRole(requirement.Role))
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ EventsExpress/Policies/RoleRequirement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EventsExpress.Db.Entities;
+using Microsoft.AspNetCore.Authorization;
+
+namespace EventsExpress.Policies
+{
+    public class RoleRequirement : IAuthorizationRequirement
+    {
+        public RoleRequirement(string role)
+        {
+            Role = role;
+        }
+
+        public string Role { get; set; }
+    }
+}
