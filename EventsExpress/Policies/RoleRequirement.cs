3df6e92605ea1ac941e5236b14fe4a93bb60a5d6
DennY <45600211+DennYats@users.noreply.github.com>
2021-04-13 21:58:59 +0300

    Written Unit tests for RoleRequirements

--- EventsExpress/Policies/RoleRequirement.cs
+++ EventsExpress/Policies/RoleRequirement.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using EventsExpress.Db.Entities;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 
 namespace EventsExpress.Policies
 {
