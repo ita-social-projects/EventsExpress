23d80aed52b6a90c01900f1c23e0ce75833a2405
DennY <45600211+DennYats@users.noreply.github.com>
2021-04-20 14:28:01 +0300

    Created PhotoController

---
+++ EventsExpress/Controllers/PhotoController.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+using EventsExpress.Core.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventsExpress.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotoController : ControllerBase
+    {
+        private readonly IPhotoService _photoService;
+
+        public PhotoController(IPhotoService photoService)
+        {
+            _photoService = photoService;
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetPreviewEventPhoto(string id)
+        {
+            string url = $"/events/{id}/preview.png";
+
+            await _photoService.GetPhotoFromAzureBlob(url);
+
+            return Ok();
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetFullEventPhoto(string id)
+        {
+            string url = $"/events/{id}/full.png";
+
+            await _photoService.GetPhotoFromAzureBlob(url);
+
+            return Ok();
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetUserPhoto(string id)
+        {
+            string url = $"/users/{id}/preview.png";
+
+            await _photoService.GetPhotoFromAzureBlob(url);
+
+            return Ok();
+        }
+    }
+}
