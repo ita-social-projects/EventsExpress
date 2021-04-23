fd1b37660ee7b29aa64025589491fc4f348134c9
Oleksandr Slobodian <55383070+carendoh@users.noreply.github.com>
2021-04-23 14:55:26 +0300

    Manage notification templates (#477)
    * Added entity for storing notification templates and service for working with the entity. Notification handlers are refactored.
    
    * Renamed files, added notification controller and service on the frontend, changed Handler tests
    
    * Implemented notification editing page
    
    * refactored code, deleted useless functionality
    
    * Fixed a number of inaccuracies. Added ViewModel for notifications
    
    * Edited notification handlers. Improved naming of files and code variables.
    
    * Fixed inaccuracies in a migration file
    
    * Excessive pagination removed
    
    * Fixed testing of notification handlers.
    
    * Solved build errors
    
    * Removed code smells
    
    * Fixed working of reset button. Added unit tests for notification template service.
    
    * Removed code smell and added tests for NotificationTemplateController
    
    * Moved 'using' directives outside the namespace in NotificationTemplateControllerTests file. Added mapper tests
    
    * Access modifiers for test classes have been changed and an additional tag has been added
    
    * Validator tests and RegisterVerificationHandler tests have been added.
    
    * Changes were undone in the package.json
    
    * The unused file was removed and the .csproj changes were undone.

---
+++ EventsExpress.Test/MapperTests/NotificationTemplateMapperTests.cs
@@ -0,0 +1,104 @@
+﻿using EventsExpress.Core.DTOs;
+using EventsExpress.Db.Entities;
+using EventsExpress.Db.Enums;
+using EventsExpress.Mapping;
+using EventsExpress.Test.MapperTests.BaseMapperTestInitializer;
+using EventsExpress.ViewModels;
+using NUnit.Framework;
+
+namespace EventsExpress.Test.MapperTests
+{
+    [TestFixture]
+    internal class NotificationTemplateMapperTests : MapperTestInitializer<NotificationTemplateMapperProfile>
+    {
+        private const NotificationProfile Id = NotificationProfile.BlockedUser;
+        private const string Title = "title";
+        private const string Subject = "subject";
+        private const string Message = "message";
+
+        private NotificationTemplate _template;
+        private NotificationTemplateDto _templateDto;
+        private EditNotificationTemplateViewModel _viewModelTemplate;
+
+        [OneTimeSetUp]
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            _template = new NotificationTemplate
+            {
+                Id = Id,
+                Title = Title,
+                Subject = Subject,
+                Message = Message,
+            };
+
+            _viewModelTemplate = new EditNotificationTemplateViewModel
+            {
+                Id = Id,
+                Subject = Subject,
+                Message = Message,
+            };
+
+            _templateDto = new NotificationTemplateDto
+            {
+                Id = Id,
+                Title = Title,
+                Subject = Subject,
+                Message = Message,
+            };
+        }
+
+        [Test]
+        public void MapperProfile_IsValidConfiguration()
+        {
+            Configuration.AssertConfigurationIsValid();
+        }
+
+        [Test]
+        public void MapperProfile_FromTemplate_To_TemplateDto_MapIsCorrect()
+        {
+            var templateDto = Mapper.Map<NotificationTemplateDto>(_template);
+
+            Assert.AreEqual(_template.Id, templateDto.Id);
+            Assert.AreEqual(_template.Title, templateDto.Title);
+            Assert.AreEqual(_template.Subject, templateDto.Subject);
+            Assert.AreEqual(_template.Message, templateDto.Message);
+        }
+
+        [Test]
+        public void MapperProfile_FromEditViewModelTemplate_To_TemplateDto_MapIsCorrect()
+        {
+            var templateDto = Mapper.Map<NotificationTemplateDto>(_viewModelTemplate);
+
+            Assert.AreEqual(_viewModelTemplate.Id, templateDto.Id);
+            Assert.AreEqual(_viewModelTemplate.Subject, templateDto.Subject);
+            Assert.AreEqual(_viewModelTemplate.Message, templateDto.Message);
+        }
+
+        [Test]
+        public void MapperProfile_FromTemplateDto_To_Template_MapIsCorrect()
+        {
+            var template = Mapper.Map<NotificationTemplate>(_templateDto);
+
+            Assert.AreEqual(_templateDto.Id, template.Id);
+            Assert.IsNull(template.Title);
+            Assert.AreEqual(_templateDto.Subject, template.Subject);
+            Assert.AreEqual(_templateDto.Message, template.Message);
+        }
+
+        [Test]
+        public void MapperProfile_FromTemplateDto_To_Template_TitleIsIgnored()
+        {
+            const string expectedTitle = "expectedTitle";
+            const string dtoTitle = "dtoTitle";
+
+            _templateDto.Title = dtoTitle;
+            _template.Title = expectedTitle;
+
+            Mapper.Map(_templateDto, _template);
+
+            Assert.AreEqual(expectedTitle, _template.Title);
+        }
+    }
+}
---
+++ EventsExpress/ClientApp/src/reducers/notification-templates/notification-templates.js
@@ -0,0 +1,12 @@
+﻿import initialState from '../../store/initialState';
+import { GET_TEMPLATES_SUCCESS } from '../../actions/notification-templates'
+
+export const reducer = (state = initialState.notificationTemplates, action) => {
+    if (action.type === GET_TEMPLATES_SUCCESS) {
+        return {
+            data: [...action.payload]
+        }
+    }
+
+    return state;
+}
