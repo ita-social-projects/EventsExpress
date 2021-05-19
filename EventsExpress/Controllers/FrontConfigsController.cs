e962d7c551934dd8cf76b1999e6b53f786353fcd
Stando <stas93007@gmail.com>
2021-05-19 15:03:14 +0300

    create view model

---
+++ EventsExpress/Controllers/FrontConfigsController.cs
@@ -0,0 +1,23 @@
+﻿namespace EventsExpress.Controllers
+{
+    using EventsExpress.ViewModels;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Options;
+
+    public class FrontConfigsController : Controller
+    {
+        private readonly IOptions<FrontConfigsViewModel> _frontConfigs;
+
+        public FrontConfigsController(IOptions<FrontConfigsViewModel> frontConfigs)
+        {
+            _frontConfigs = frontConfigs;
+        }
+
+        public IActionResult GetConfigs()
+        {
+            _ = new FrontConfigsViewModel();
+            FrontConfigsViewModel configResult = _frontConfigs.Value;
+            return Ok(configResult);
+        }
+    }
+}
---
+++ EventsExpress/ViewModels/FrontConfigsViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace EventsExpress.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class FrontConfigsViewModel
+    {
+        public string FACEBOOK_CLIENT_ID { get; set; }
+
+        public string GOOGLE_CLIENT_ID { get; set; }
+
+        public string TWITTER_CALLBACK_URL { get; set; }
+
+        public string TWITTER_CONSUMER_KEY { get; set; }
+
+        public string TWITTER_CONSUMER_SECRET { get; set; }
+    }
+}
