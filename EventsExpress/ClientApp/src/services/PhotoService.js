1e39be3a1ee974d0f5b679813b98199d05a767e7
Oleksandr Dvoretskyi <sashadv7@gmail.com>
2021-06-03 17:42:11 +0300

    Fixed and improved functionality related to the DropZone field. Fixed UI issues with displaying event image and user avatar (different crop shapes). Improved styles

--- EventsExpress/ClientApp/src/services/PhotoService.js
+++ EventsExpress/ClientApp/src/services/PhotoService.js
@@ -6,4 +6,6 @@ export default class PhotoService {
     getPreviewEventPhoto = id => baseService.getPhoto(`photo/GetPreviewEventPhoto?id=${id}`);
 
     getFullEventPhoto = id => baseService.getPhoto(`photo/GetFullEventPhoto?id=${id}`);
+
+    getUserPhoto = id => baseService.getPhoto(`photo/GetUserPhoto?id=${id}`);
 }
