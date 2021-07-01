d1f21d50206480de69bebe80d5201dc89b0f6d9d
MaxYashan1899 <Xamax1899@gmail.com>
2021-07-01 14:41:32 +0300

    Deleted label description

--- EventsExpress/ClientApp/src/components/contactAdmin/contactAdmin-component.js
+++ EventsExpress/ClientApp/src/components/contactAdmin/contactAdmin-component.js
@@ -83,7 +83,6 @@ class ContactAdmin extends Component {
                                 className="form-control"
                                 component={renderTextArea}
                                 type="input"
-                                label="Description"
                                />
                         </div>
                         {error && <ErrorMessages error={error} className="text-center" />}
