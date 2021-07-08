c91bacaff902f9d4851b0f1cf738b850e2fe5422
Oleksandr Slobodian <55383070+carendoh@users.noreply.github.com>
2021-07-08 14:46:59 +0300

    App working resumed (#693)
    * App working resumed. Fixed wrong import paths and file names
    
    * Improved file naming

--- EventsExpress/ClientApp/src/components/contactAdmin/contactAdmin-component.js
+++ EventsExpress/ClientApp/src/components/contactAdmin/contactAdmin-component.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import { reduxForm, Field, getFormValues } from 'redux-form';
 import { connect } from 'react-redux';
 import Button from '@material-ui/core/Button';
-import { renderTextField, renderTextArea  } from '../helpers/form-helpers';
+import { renderTextField, renderTextArea } from '../helpers/form-helpers';
 import ErrorMessages from '../shared/errorMessage';
-import issueTypeEnum from '../../constants/issueTypeEnum ';
+import issueTypeEnum from '../../constants/issue-type-enum';
 import { isValidEmail } from '../helpers/validators/email-address-validator';
 import { maxLength30 } from '../helpers/validators/min-max-length-validators';
 import { fieldIsRequired } from '../helpers/validators/required-fields-validator';
@@ -17,7 +17,7 @@ const validate = values => {
         'description'
     ];
     if (maxLength30(values.title)) {
-        errors.title = `Title should be less 30 symbols`;
+        errors.title = 'Title should be less 30 symbols';
     }
 
     return {
@@ -32,11 +32,11 @@ class ContactAdmin extends Component {
     render() {
         const { pristine, reset, submitting, error } = this.props;
         return (
-            <div id='notfound'>
-                <div className='notfound'>
-                    <h1 className='f1'>{'Contact Us'}</h1>
+            <div id="notfound">
+                <div className="notfound">
+                    <h1 className="f1">Contact Us</h1>
                     <form className="notfound-404" onSubmit={this.props.handleSubmit}>
-                        <div className="box text text-2 pl-md-4 " >
+                        <div className="box text text-2 pl-md-4 ">
                             {(this.props.user.role === "User")
                                 ? <Field
                                     name="email"
@@ -52,20 +52,20 @@ class ContactAdmin extends Component {
                                     label="Your e-mail:"
                                 />
                             }
-                            <p></p><p></p><p></p>
+                            <p /><p /><p />
                             <div className="text-left mb-2">Problem Type</div>
                             <Field
-                                name='subject'
+                                name="subject"
                                 className="form-control"
                                 component="select"
                                 parse={value => Number(value)}
                             >
-                                <option value={issueTypeEnum.NewCategory}>New Category</option>;
-                                <option value={issueTypeEnum.BugReport}>Bug Report</option>;
-                                <option value={issueTypeEnum.BadEvent}>Bad Event</option>;
-                                <option value={issueTypeEnum.BadUser}>Bad User</option>;
-                                <option value={issueTypeEnum.Other}>Other</option>;
-                             </Field>
+                                <option value={issueTypeEnum.NewCategory}>New Category</option>
+                                <option value={issueTypeEnum.BugReport}>Bug Report</option>
+                                <option value={issueTypeEnum.BadEvent}>Bad Event</option>
+                                <option value={issueTypeEnum.BadUser}>Bad User</option>
+                                <option value={issueTypeEnum.Other}>Other</option>
+                            </Field>
 
                             {(this.props.form_values !== undefined
                                 && this.props.form_values.subject == issueTypeEnum.Other
@@ -73,17 +73,17 @@ class ContactAdmin extends Component {
                                     name="title"
                                     className="form-control"
                                     component={renderTextField}
-                                label="Enter problem type:"
+                                    label="Enter problem type:"
                                 />
                             )}
 
-                            <p></p><p></p><p></p>
+                            <p /><p /><p />
                             <Field
-                                name='description'
+                                name="description"
                                 className="form-control"
                                 component={renderTextArea}
                                 type="input"
-                               />
+                            />
                         </div>
                         {error && <ErrorMessages error={error} className="text-center" />}
                         <Button
@@ -91,14 +91,14 @@ class ContactAdmin extends Component {
                             color="primary"
                             disabled={pristine || submitting}>
                             Submit
-                             </Button>
+                        </Button>
                         <Button
                             type="button"
                             color="primary"
                             disabled={pristine || submitting}
                             onClick={reset}>
                             Clear
-                             </Button>
+                        </Button>
                     </form>
                 </div>
             </div>
@@ -114,7 +114,7 @@ const mapStateToProps = (state) => {
 }
 
 export default connect(mapStateToProps)(reduxForm({
-    form: "ContactAdmin",
+    form: 'ContactAdmin',
     validate,
     enableReinitialize: true
 })(ContactAdmin));
