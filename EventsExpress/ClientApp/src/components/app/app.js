9859f00048ba4432d23000397898076580fd098e
maxrok98 <max.rok.98@gmail.com>
2021-08-13 14:45:18 +0300

    Add functional logout button on landing page

--- EventsExpress/ClientApp/src/components/app/app.js
+++ EventsExpress/ClientApp/src/components/app/app.js
@@ -5,7 +5,7 @@ import {
     Redirect,
     Switch
 } from 'react-router-dom';
-import Landing from '../landing'
+import LandingWrapper from '../../containers/landing'
 import Home from '../home';
 import Issues from '../contactAdmin/issues';
 import Profile from '../profile';
@@ -50,7 +50,7 @@ class App extends Component {
         return (
             <BrowserRouter>
                 <Switch>
-                    <Route path="/landing" component={Landing} />
+                    <Route path="/landing" component={LandingWrapper} />
                     <Route
                         exact
                         path="/"
