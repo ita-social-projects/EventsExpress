5dfecd2d70280c415043001b5a9ee7f44510adb0
Hryn Oleksandr <Apostall107@users.noreply.github.com>
2021-10-12 08:30:20 +0300

    Fixed formating style of stepper files.

--- EventsExpress/ClientApp/src/components/stepper/Stepper.js
+++ EventsExpress/ClientApp/src/components/stepper/Stepper.js
@@ -1,118 +1,113 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import "./Stepper.css";
 
 export default class Stepper extends Component {
-    constructor() {
-        super();
-        this.state = { steps: [] };
+  constructor() {
+    super();
+    this.state = { steps: [] };
+  }
+
+  componentDidMount() {
+    const { steps, currentStepNumber } = this.props;
+    const stepsState = steps.map((step, index) => {
+      const stepObj = {};
+      stepObj.description = step;
+      stepObj.highlighted = index === 0 ? true : false;
+      stepObj.selected = index === 0 ? true : false;
+      stepObj.completed = false;
+      return stepObj;
+    });
+
+    const currentSteps = this.updateStep(currentStepNumber, stepsState);
+    this.setState({ steps: currentSteps });
+  }
+
+  componentDidUpdate(prevProps) {
+    const { steps } = this.state;
+    const currentSteps = this.updateStep(this.props.currentStepNumber, steps);
+
+    if (prevProps.currentStepNumber !== this.props.currentStepNumber) {
+      this.setState({ steps: currentSteps });
     }
+  }
 
-    componentDidMount() {
-        const { steps, currentStepNumber } = this.props;
-        const stepsState = steps.map((step, index) => {
-            const stepObj = {};
-            stepObj.description = step;
-            stepObj.highlighted = index === 0 ? true : false;
-            stepObj.selected = index === 0 ? true : false;
-            stepObj.completed = false;
-            return stepObj;
-        });
+  updateStep(stepNumber, steps) {
+    const newSteps = [...steps];
+    let stepCounter = 0;
 
-        const currentSteps = this.updateStep(currentStepNumber, stepsState);
-        this.setState({ steps: currentSteps });
+    while (stepCounter < newSteps.length) {
+      // Current step
+      if (stepCounter === stepNumber) {
+        newSteps[stepCounter] = {
+          ...newSteps[stepCounter],
+          highlighted: true,
+          selected: true,
+          completed: false,
+        };
+        stepCounter++;
+      }
+      // Prev step
+      else if (stepCounter < stepNumber) {
+        newSteps[stepCounter] = {
+          ...newSteps[stepCounter],
+          highlighted: false,
+          selected: true,
+          completed: true,
+        };
+        stepCounter++;
+      }
+      // Next steps
+      else {
+        newSteps[stepCounter] = {
+          ...newSteps[stepCounter],
+          highlighted: false,
+          selected: false,
+          completed: false,
+        };
+        stepCounter++;
+      }
     }
+    return newSteps;
+  }
 
-    componentDidUpdate(prevProps) {
-        const { steps } = this.state;
-        const currentSteps = this.updateStep(this.props.currentStepNumber, steps);
+  render() {
+    const { stepColor } = this.props;
+    const { steps } = this.state;
+    const stepsDisplay = steps.map((step, index) => {
+      return (
+        <div className="step-wrapper" key={index}>
+          <div
+            className={`step-number ${
+              step.selected ? "step-number-selected" : "step-number-disabled"
+            }`}
+            style={{
+              background: `${step.selected ? stepColor : "none"}`,
+            }}
+          >
+            {step.completed ? <span>&#10004;</span> : index + 1}
+          </div>
 
-        if (prevProps.currentStepNumber !== this.props.currentStepNumber) { this.setState({ steps: currentSteps }); }
-    }
+          <div
+            className={`step-description ${
+              step.highlighted && "step-description-active"
+            }`}
+          >
+            {step.description}
+          </div>
+          {index !== steps.length - 1 && (
+            <div className={`divider-line divider-line-${steps.length}`} />
+          )}
+        </div>
+      );
+    });
 
-    updateStep(stepNumber, steps) {
-        const newSteps = [...steps];
-        let stepCounter = 0;
-
-        while (stepCounter < newSteps.length) {
-            // Current step
-            if (stepCounter === stepNumber) {
-                newSteps[stepCounter] = {
-                    ...newSteps[stepCounter],
-                    highlighted: true,
-                    selected: true,
-                    completed: false
-                };
-                stepCounter++;
-            }
-            // Prev step
-            else if (stepCounter < stepNumber) {
-                newSteps[stepCounter] = {
-                    ...newSteps[stepCounter],
-                    highlighted: false,
-                    selected: true,
-                    completed: true
-                };
-                stepCounter++;
-            }
-            // Next steps
-            else {
-                newSteps[stepCounter] = {
-                    ...newSteps[stepCounter],
-                    highlighted: false,
-                    selected: false,
-                    completed: false
-                };
-                stepCounter++;
-            }
-        }
-        return newSteps;
-    }
-
-
-    render() {
-        const { stepColor } = this.props;
-        const { steps } = this.state;
-        const stepsDisplay = steps.map((step, index) => {
-            return (
-                <div className="step-wrapper" key={index}>
-                    <div
-                        className={
-                            `step-number ${step.selected
-                                ? "step-number-selected"
-                                : "step-number-disabled"}`
-                        }
-                        style={
-                            {
-                                background: `${step.selected
-                                    ? stepColor
-                                    : "none"}`
-                            }
-                        }
-                    >
-                        {
-                            step.completed
-                                ? <span>&#10004;</span>
-                                : index + 1
-                        }
-                    </div>
-
-                    <div className={`step-description ${step.highlighted && "step-description-active"}`}>
-                        {step.description}
-                    </div>
-                    {
-                        index !== steps.length - 1 && (<div className={`divider-line divider-line-${steps.length}`} />)
-                    }
-                </div>
-            );
-        });
-
-        return (<div className="stepper-wrapper-horizontal">{stepsDisplay}</div>);
-    }
+    return <div className="stepper-wrapper-horizontal">{stepsDisplay}</div>;
+  }
 }
 
-Stepper.propTypes ={
-    currentStepNumber: PropTypes.number.isRequired,
-    steps: PropTypes.array.isRequired,
-    stepColor: PropTypes.string.isRequired
+Stepper.propTypes = {
+  currentStepNumber: PropTypes.number.isRequired,
+  steps: PropTypes.array.isRequired,
+  stepColor: PropTypes.string.isRequired,
 };
