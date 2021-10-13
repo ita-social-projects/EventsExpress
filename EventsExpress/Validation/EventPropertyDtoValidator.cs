012877e1755f4755ad8bd0c13d18ba025b939b6c
pirogg6 <pirogg6@gmail.com>
2021-10-13 12:21:58 +0300

    ValidationChanges

---
+++ EventsExpress/Validation/EventPropertyDtoValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using EventsExpress.Core.DTOs;
+using EventsExpress.Db.Entities;
+using FluentValidation;
+
+namespace EventsExpress.Validation
+{
+    public class EventPropertyDtoValidator : AbstractValidator<EventDto>
+    {
+        public EventPropertyDtoValidator()
+        {
+            RuleFor(x => x.Title).NotEmpty().WithMessage("Field is required!");
+            RuleFor(x => x.Title).MaximumLength(60).WithMessage("Title length exceeded the recommended length of 60 character!");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Field is required!");
+            RuleFor(x => x.DateFrom).NotEmpty().WithMessage("Field is required!");
+            RuleFor(x => x.DateFrom).GreaterThan(DateTime.Today).WithMessage("Date from must be older than the current date!");
+            RuleFor(x => x.DateTo).NotEmpty().WithMessage("Field is required!");
+            RuleFor(x => x.DateTo).GreaterThan(x => x.DateFrom).WithMessage("Date to must be older than date from!");
+        }
+    }
+}
--- EventsExpress/Validation/EventsDtoValidator.cs
+++ EventsExpress/Validation/EventsDtoValidator.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using EventsExpress.Core.DTOs;
 using EventsExpress.Db.Entities;
+using EventsExpress.Db.Enums;
 using FluentValidation;
 
 namespace EventsExpress.Validation
 {
-    public class EventDtoValidator : AbstractValidator<EventDto>
+    public class EventsDtoValidator : AbstractValidator<EventDto>
     {
-        public EventDtoValidator()
+        public EventsDtoValidator()
         {
             RuleFor(x => x.Title).NotEmpty().WithMessage("Field is required!");
             RuleFor(x => x.Title).MaximumLength(60).WithMessage("Title length exceeded the recommended length of 60 character!");
@@ -22,6 +23,11 @@ namespace EventsExpress.Validation
             {
                 RuleFor(x => x.Frequency).GreaterThan(0).WithMessage("Incorrect frequency!");
             });
+            When(x => x.Point == null, () =>
+            {
+                RuleFor(x => x.OnlineMeeting).NotEmpty().OverridePropertyName("location.type").WithMessage("Field is required!");
+            });
+            RuleForEach(x => x.Events).SetValidator(new EventPropertyDtoValidator());
         }
     }
 }
