e49b4705bf9bae32a2f218a5ca9529104d4de59e
Neckral <vlad.rusnak2@gmail.com>
2021-10-21 15:10:29 +0300

    fixedBuild

--- EventsExpress.Core/Services/AuthService.cs
+++ EventsExpress.Core/Services/AuthService.cs
@@ -172,9 +172,9 @@ namespace EventsExpress.Core.Services
             return new AuthenticateResponseModel(jwtToken, refreshToken.Token);
         }
 
-        public async Task<AuthenticateResponseModel> EmailConfirmAndAuthenticate(Guid accountId, string token)
+        public async Task<AuthenticateResponseModel> EmailConfirmAndAuthenticate(Guid accountId1, string token)
         {
-            var accountId = Context.AuthLocal.First(al => al.Id == authLocalId).AccountId;
+            var accountId = Context.AuthLocal.First(al => al.Id == accountId1).AccountId;
             var userToken = Context.UserTokens
                             .First(rt => rt.Token == token && rt.AccountId == accountId);
 
