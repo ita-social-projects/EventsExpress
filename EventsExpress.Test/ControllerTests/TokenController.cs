59e327c8a569411c49bca835237aaa222952b2ac
Neckral <vlad.rusnak2@gmail.com>
2021-10-26 20:47:02 +0300

    SaveBeforeSwitching

--- EventsExpress.Test/ControllerTests/TokenController.cs
+++ EventsExpress.Test/ControllerTests/TokenController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using AutoMapper;
 using EventsExpress.Controllers;
 using EventsExpress.Core.DTOs;
@@ -26,7 +28,7 @@ using NUnit.Framework;
 namespace EventsExpress.Test.ControllerTests
 {
     [TestFixture]
-    internal class TokenServiceController : TestInitializer
+    internal class TokenController : TestInitializer
     {
         private Mock<IJwtSigningEncodingKey> _mockSigningEncodingKey;
         private Mock<IOptions<JwtOptionsModel>> _mockJwtOptions;
@@ -39,7 +41,7 @@ namespace EventsExpress.Test.ControllerTests
         private User _existingUser;
         private string _token;
 
-        private TokenController _tokenController;
+        private Controllers.TokenController _tokenController;
 
         [SetUp]
         protected override void Initialize()
@@ -81,12 +83,17 @@ namespace EventsExpress.Test.ControllerTests
             Context.Accounts.Add(_existingAccount);
             Context.SaveChanges();
 
-            _tokenController = new TokenController(_tokenService);
+            _tokenController = new Controllers.TokenController(_tokenService);
         }
 
         [Test]
         public void Refresh_Correct_ReturnOk()
         {
+            _tokenController.ControllerContext = new ControllerContext();
+            _tokenController.ControllerContext.HttpContext = new DefaultHttpContext();
+            _tokenController.ControllerContext.HttpContext.Request.Cookies.Keys.Add("refreshToken");
+
+            // _tokenController.ControllerContext.HttpContext.Request.Cookies["refreshToken"] = _token;
             var res = _tokenController.Refresh();
             Assert.IsNotInstanceOf<OkObjectResult>(res);
         }
@@ -94,6 +101,8 @@ namespace EventsExpress.Test.ControllerTests
         [Test]
         public void Revoke_Correct_ReturnOk()
         {
+            _tokenController.ControllerContext = new ControllerContext();
+            _tokenController.ControllerContext.HttpContext = new DefaultHttpContext();
             var res = _tokenController.Revoke();
             Assert.IsNotInstanceOf<OkObjectResult>(res);
         }
