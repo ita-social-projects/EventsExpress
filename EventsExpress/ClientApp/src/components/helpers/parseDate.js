e0fbbfd8efb7295fc23570fa1849fc8a92d70add
Neckral <vlad.rusnak2@gmail.com>
2021-10-27 17:38:38 +0300

    ChangedOldDateParseToNewWithMomentJS

--- EventsExpress/ClientApp/src/components/helpers/parseDate.js
+++ EventsExpress/ClientApp/src/components/helpers/parseDate.js
@@ -1,12 +1,5 @@
-﻿export function parseDate(dateFrom) {
-    let date = new Date(dateFrom);
-    let weekday = date.toLocaleString('en-us', { weekday: 'short' }).toUpperCase();
-    let month = date.toLocaleString('en-us', { month: 'short' }).toUpperCase();
-    let ddate = date.getDate();
-    let hours = date.toLocaleString('en-us', { hours: 'short' })
-    let ampm = hours.slice(hours.length - 2, hours.length)
-    hours = hours.slice(12, hours.length - 6);
-
-    let gmt = date.getTimezoneOffset();
-    return weekday + ', ' + month + ' ' + ddate + ' @' + hours + ' ' + ampm + ' GMT' + gmt/60;
-}
+﻿import moment from 'moment';
+export function parseDate(dateFrom) {
+    let result = moment(dateFrom).format('ddd, MMM D LT [GMT]Z');
+    return result.toUpperCase();
+} 
