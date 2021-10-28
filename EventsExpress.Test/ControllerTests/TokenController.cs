a20d1a8e5fde65607e025a91ff156cb926c0f322
Neckral <vlad.rusnak2@gmail.com>
2021-10-28 09:08:20 +0300

    savedBeforSwitching

--- EventsExpress.Test/ControllerTests/TokenController.cs
+++ EventsExpress.Test/ControllerTests/TokenController.cs
@@ -28,62 +28,38 @@ using NUnit.Framework;
 namespace EventsExpress.Test.ControllerTests
 {
     [TestFixture]
-    internal class TokenController : TestInitializer
+    internal class TokenController
     {
-        private Mock<IJwtSigningEncodingKey> _mockSigningEncodingKey;
-        private Mock<IOptions<JwtOptionsModel>> _mockJwtOptions;
-        private Mock<IHttpContextAccessor> _httpContextAccessor;
-        private Mock<IIpProviderService> _iIpProviderService;
-
-        private TokenService _tokenService;
-        private List<Claim> _claims;
-        private Account _existingAccount;
-        private User _existingUser;
-        private string _token;
-
+        private Mock<ITokenService> _tokenService;
+        private Mock<DefaultHttpContext> _httpContext;
         private Controllers.TokenController _tokenController;
 
         [SetUp]
-        protected override void Initialize()
+        protected void Initialize()
         {
-            base.Initialize();
+            _tokenService = new Mock<ITokenService>();
+            _httpContext = new Mock<DefaultHttpContext>();
+            _tokenController = new Controllers.TokenController(_tokenService.Object);
+        }
 
-            _mockJwtOptions = new Mock<IOptions<JwtOptionsModel>>();
-            _mockSigningEncodingKey = new Mock<IJwtSigningEncodingKey>();
-            _httpContextAccessor = new Mock<IHttpContextAccessor>();
-            _iIpProviderService = new Mock<IIpProviderService>();
+        [Test]
+        public void Refresh_Correct_ReturnOk()
+        {
+            _tokenService.Setup(s => s.RefreshToken("token").Result).Returns(new AuthenticateResponseModel("jwtToken", "refreshToken"));
+            _httpContext.Setup(opt => opt.Request.Cookies["refreshToken"]).Returns("refreshToken");
+            _tokenController.ControllerContext = new ControllerContext();
+            _tokenController.ControllerContext.HttpContext = _httpContext.Object;
+            var res = _tokenController.Refresh();
+            Assert.IsNotInstanceOf<OkObjectResult>(res);
+        }
 
-            _tokenService = new TokenService(
-                Context,
-                MockMapper.Object,
-                _mockJwtOptions.Object,
-                _mockSigningEncodingKey.Object,
-                _httpContextAccessor.Object,
-                _iIpProviderService.Object);
-
-            _token = Guid.NewGuid().ToString();
-
-            _existingUser = new User
-            {
-                Id = Guid.NewGuid(),
-                Account = _existingAccount,
-            };
-
-            _existingAccount = new Account
-            {
-                Id = Guid.NewGuid(),
-                UserId = _existingUser.Id,
-                AccountRoles = new[] { new AccountRole { RoleId = Db.Enums.Role.User } },
-                RefreshTokens = new List<UserToken> { new UserToken { Token = _token, Type = TokenType.RefreshToken, Expires = DateTime.Now.AddDays(7), Created = DateTime.Now } },
-            };
-
-            _claims = new List<Claim> { new Claim(ClaimTypes.Name, $"{_existingAccount.UserId}") };
-
-            Context.Users.Add(_existingUser);
-            Context.Accounts.Add(_existingAccount);
-            Context.SaveChanges();
-
-            _tokenController = new Controllers.TokenController(_tokenService);
+        [Test]
+        public void Revoke_Correct_ReturnOk()
+        {
+            _tokenController.ControllerContext = new ControllerContext();
+            _tokenController.ControllerContext.HttpContext = new DefaultHttpContext();
+            var res = _tokenController.Revoke();
+            Assert.IsNotInstanceOf<OkObjectResult>(res);
         }
     }
 }
