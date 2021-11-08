50aac2e7e9d697375a501eec4f57ec5e33343b1e
Hryn Oleksandr <Apostall107@users.noreply.github.com>
2021-11-08 04:57:51 +0200

    Version with window saved pics.

--- EventsExpress/ClientApp/src/components/landing/landing.js
+++ EventsExpress/ClientApp/src/components/landing/landing.js
@@ -5,44 +5,36 @@ import EventService from "../../services/EventService";
 import ModalWind from "../modal-wind";
 import AuthComponent from "../../security/authComponent";
 import "./landing.css";
-import LandingHeadArticle from "./LandingHeadArticle";
-import ArticleBg1 from "./landing-head-articles/ArticleBg1";
-import ArticleBg2 from "./landing-head-articles/ArticleBg2";
-import ArticleBg3 from "./landing-head-articles/ArticleBg3";
-import ArticleBg4 from "./landing-head-articles/ArticleBg4";
-import ArticleBg5 from "./landing-head-articles/ArticleBg5";
-import ArticleBg6 from "./landing-head-articles/ArticleBg6";
-import ArticleBg7 from "./landing-head-articles/ArticleBg7";
-import ArticleBg8 from "./landing-head-articles/ArticleBg8";
-
-
-/* const ArticleBg1Promise = import("./landing-head-articles/ArticleBg1");
-const ArticleBg1 = React.lazy(() => ArticleBg1Promise);
-const ArticleBg2Promise = import("./landing-head-articles/ArticleBg2");
-const ArticleBg2 = React.lazy(() => ArticleBg2Promise);
-const ArticleBg3Promise = import("./landing-head-articles/ArticleBg3");
-const ArticleBg3 = React.lazy(() => ArticleBg3Promise);
-const ArticleBg4Promise = import("./landing-head-articles/ArticleBg4");
-const ArticleBg4 = React.lazy(() => ArticleBg4Promise);
-const ArticleBg5Promise = import("./landing-head-articles/ArticleBg5");
-const ArticleBg5 = React.lazy(() => ArticleBg5Promise);
-const ArticleBg6Promise = import("./landing-head-articles/ArticleBg6");
-const ArticleBg6 = React.lazy(() => ArticleBg6Promise);
-const ArticleBg7Promise = import("./landing-head-articles/ArticleBg7");
-const ArticleBg7 = React.lazy(() => ArticleBg7Promise);
-const ArticleBg8Promise = import("./landing-head-articles/ArticleBg8");
-const ArticleBg8 = React.lazy(() => ArticleBg8Promise);
- */
-
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
+import image1 from "./landing-images/1.jpg";
+import image2 from "./landing-images/2.jpg";
+import image3 from "./landing-images/3.jpg";
+import image4 from "./landing-images/4.jpg";
+import image5 from "./landing-images/5.jpg";
+import image6 from "./landing-images/6.jpg";
+import image7 from "./landing-images/7.jpg";
+import image8 from "./landing-images/8.jpg";
+import SpinnerWrapper from "../../containers/spinner";
 
 const eventService = new EventService();
 
+const imagesPreload = [
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+];
 export default class Landing extends Component {
   constructor(props) {
     super(props);
     this.state = {
       currentBg: 1,
-      ImagesLoadingCounter: 0,
+      currentImage: image1,
       events: [],
     };
   }
@@ -66,9 +58,16 @@ export default class Landing extends Component {
       this.setState({ events: this.splitDataIntoBlocks(events) });
     }
 
+    imagesPreload.forEach((image) => {
+      const newImage = new Image();
+      newImage.src = image;
+      window[image] = newImage;
+    });
+
     this.interval = setInterval(() => {
       this.setState({
-        currentBg: (this.state.currentBg % 8) + 1,
+        currentImage:
+          imagesPreload[Math.floor(Math.random() * imagesPreload.length)],
       });
     }, 5000);
   }
@@ -82,120 +81,147 @@ export default class Landing extends Component {
   );
 
   render() {
-    const { events, currentBg } = this.state;
+    const { events, currentBg, currentImage } = this.state;
     const carouselNavIsVisible = events.length > 1;
     const { onLogoutClick } = this.props;
     const { id } = this.props.user;
 
     return (
       <>
-        <div className="main">
-                   {currentBg === 1 && (
-            <ArticleBg1 onLogoutClick={onLogoutClick} id={id} />
-          )}
-          {currentBg === 2 && (
-            <ArticleBg2 onLogoutClick={onLogoutClick} id={id} />
-          )}
-          {currentBg === 3 && (
-            <ArticleBg3 onLogoutClick={onLogoutClick} id={id} />
-          )}
-          {currentBg === 4 && (
-            <ArticleBg4 onLogoutClick={onLogoutClick} id={id} />
-          )}
-          {currentBg === 5 && (
-            <ArticleBg5 onLogoutClick={onLogoutClick} id={id} />
-          )}
-          {currentBg === 6 && (
-            <ArticleBg6 onLogoutClick={onLogoutClick} id={id} />
-          )}
-          {currentBg === 7 && (
-            <ArticleBg7 onLogoutClick={onLogoutClick} id={id} />
-          )}
-          {currentBg === 8 && (
-            <ArticleBg8 onLogoutClick={onLogoutClick} id={id} />
-          )} 
-
-
-
-
-          <article className="works-article text-center">
-            <div className="works-title">
-              <h2>How EventsExpress Works</h2>
-            </div>
-            <div className="icons-wrapper">
-              <div>
-                <i className="fas fa-hand-pointer"></i>
-                <h3>Search Events</h3>
-              </div>
-              <div>
-                <i className="fas fa-user-plus"></i>
-                <h3>Create Your Own Event</h3>
-              </div>
-              <div>
-                <i className="fas fa-search"></i>
-                <h3>Find Events Partners</h3>
-              </div>
-              <div>
-                <i className="far fa-smile"></i>
-                <h3>Get Connected</h3>
-              </div>
-              <div>
-                <i className="fas fa-users"></i>
-                <h3>Have Fun Together</h3>
-              </div>
-            </div>
-            <AuthComponent onlyAnonymous>
-              <div className="text-center">
-                <ModalWind
-                  renderButton={(action) => (
-                    <button
-                      className="btn btn-warning"
-                      onClick={() => action()}
-                    >
-                      Join EventsExpress
-                    </button>
-                  )}
-                />
-              </div>
-            </AuthComponent>
-          </article>
-          {events.length !== 0 && (
-            <article className="events-article">
-              <div className="row">
+        {" "}
+        <SpinnerWrapper showContent={window.length < imagesPreload.length}>
+          <div className="main">
+            <article
+              style={{
+                backgroundImage: `url(${currentImage})`,
+                backgroundPosition: "center",
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+              }}
+            >
+              <nav className="row">
                 <div className="col-md-10">
-                  <h3>Upcoming events</h3>
+                  <h1>EventsExpress</h1>
                 </div>
-                <div style={{ textAlign: "right" }} className="col-md-2">
-                  <a href="/home/events">Explore more events</a>
+                <AuthComponent onlyAnonymous>
+                  <div className="col-md-1">
+                    {!id && (
+                      <ModalWind
+                        renderButton={(action) => (
+                          <Button
+                            className="mt-5 btn btn-warning"
+                            variant="contained"
+                            onClick={action}
+                          >
+                            Sign In/Up
+                          </Button>
+                        )}
+                      />
+                    )}
+                  </div>
+                </AuthComponent>
+                <AuthComponent>
+                  <div className="col-md-2 text-right">
+                    <div onClick={onLogoutClick} className="btn">
+                      Log out
+                    </div>
+                  </div>
+                </AuthComponent>
+              </nav>
+              <div className="button-container text-center">
+                <h2>What do you want to do?</h2>
+                <div className="buttons">
+                  <button
+                    className="btn btn-warning"
+                    onClick={this.handleClick}
+                  >
+                    Create event
+                  </button>
+                  <Link to={"home/events"} className="btn btn-warning">
+                    Find event
+                  </Link>
                 </div>
               </div>
-              <div className="carousel-wrapper text-center">
-                <Carousel
-                  autoPlay={false}
-                  animation={"slide"}
-                  interval={1000}
-                  indicators={false}
-                  navButtonsAlwaysVisible={carouselNavIsVisible}
-                  navButtonsAlwaysInvisible={!carouselNavIsVisible}
-                  NextIcon={
-                    <i
-                      style={{ width: 32 + "px", height: 32 + "px" }}
-                      className="fas fa-angle-right"
-                    ></i>
-                  }
-                  PrevIcon={
-                    <i
-                      style={{ width: 32 + "px", height: 32 + "px" }}
-                      className="fas fa-angle-left"
-                    ></i>
-                  }
-                >
-                  {events.map((block) => this.renderCarouselBlock(block))}
-                </Carousel>
+            </article>{" "}
+            <article className="works-article text-center">
+              <div className="works-title">
+                <h2>How EventsExpress Works</h2>
               </div>
+              <div className="icons-wrapper">
+                <div>
+                  <i className="fas fa-hand-pointer"></i>
+                  <h3>Search Events</h3>
+                </div>
+                <div>
+                  <i className="fas fa-user-plus"></i>
+                  <h3>Create Your Own Event</h3>
+                </div>
+                <div>
+                  <i className="fas fa-search"></i>
+                  <h3>Find Events Partners</h3>
+                </div>
+                <div>
+                  <i className="far fa-smile"></i>
+                  <h3>Get Connected</h3>
+                </div>
+                <div>
+                  <i className="fas fa-users"></i>
+                  <h3>Have Fun Together</h3>
+                </div>
+              </div>
+              <AuthComponent onlyAnonymous>
+                <div className="text-center">
+                  <ModalWind
+                    renderButton={(action) => (
+                      <button
+                        className="btn btn-warning"
+                        onClick={() => action()}
+                      >
+                        Join EventsExpress
+                      </button>
+                    )}
+                  />
+                </div>
+              </AuthComponent>
             </article>
-          )}
-        </div>
+            {events.length !== 0 && (
+              <article className="events-article">
+                <div className="row">
+                  <div className="col-md-10">
+                    <h3>Upcoming events</h3>
+                  </div>
+                  <div style={{ textAlign: "right" }} className="col-md-2">
+                    <a href="/home/events">Explore more events</a>
+                  </div>
+                </div>
+                <div className="carousel-wrapper text-center">
+                  <Carousel
+                    autoPlay={false}
+                    animation={"slide"}
+                    interval={1000}
+                    indicators={false}
+                    navButtonsAlwaysVisible={carouselNavIsVisible}
+                    navButtonsAlwaysInvisible={!carouselNavIsVisible}
+                    NextIcon={
+                      <i
+                        style={{ width: 32 + "px", height: 32 + "px" }}
+                        className="fas fa-angle-right"
+                      ></i>
+                    }
+                    PrevIcon={
+                      <i
+                        style={{ width: 32 + "px", height: 32 + "px" }}
+                        className="fas fa-angle-left"
+                      ></i>
+                    }
+                  >
+                    {events.map((block) => this.renderCarouselBlock(block))}
+                  </Carousel>
+                </div>
+              </article>
+            )}
+          </div>
+        </SpinnerWrapper>
       </>
     );
   }
