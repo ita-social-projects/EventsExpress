1634edc1478cd29f92a41d63406584da839c2430
Hryn Oleksandr <Apostall107@users.noreply.github.com>
2021-11-17 16:15:33 +0200

    Use of moment.

--- EventsExpress/ClientApp/src/components/helpers/form-helpers/parseEuDate.js
+++ EventsExpress/ClientApp/src/components/helpers/form-helpers/parseEuDate.js
@@ -1,19 +1,13 @@
+import moment from "moment";
+
+
 const parseEuDate = (value) => {
   if (!value) {
     return value;
   }
 
-  //if date selected with date pick
-  if (value.toString().length > 12) {
-    return value;
-  }
-  // keyboard input
-  const onlyNums = value.toString().replace(/[^D]+/g, "");
-
-  const day = onlyNums.slice(0, 2);
-  const month = onlyNums.slice(2, 4);
-  const year = onlyNums.slice(4, 8);
-  return `${year}-${month}-${day}T00:00:00.000Z`;
+  return moment(value, "DD-MM-YYYY").toISOString();
 };
 
 export default parseEuDate;
+
