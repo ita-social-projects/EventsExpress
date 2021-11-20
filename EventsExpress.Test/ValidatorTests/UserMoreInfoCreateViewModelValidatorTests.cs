b903cdf78ef9c4d208cd0eedf50f95eb795b4219
InnaShapovalova <74597261+InnaShapovalova@users.noreply.github.com>
2021-11-20 12:37:59 +0200

    Added tests for validator

---
+++ EventsExpress.Test/ValidatorTests/UserMoreInfoCreateViewModelValidatorTests.cs
@@ -0,0 +1,38 @@
+﻿namespace EventsExpress.Test.ValidatorTests
+{
+    using System;
+    using EventsExpress.Db.Entities;
+    using EventsExpress.Validation;
+    using EventsExpress.ViewModels;
+    using FluentValidation;
+    using FluentValidation.TestHelper;
+    using NUnit.Framework;
+
+    [TestFixture]
+    internal class UserMoreInfoCreateViewModelValidatorTests
+    {
+        private IValidator<UserMoreInfoCreateViewModel> validator;
+        private UserMoreInfoCreateViewModel userMoreInfo;
+
+        [SetUp]
+        public void Setup()
+        {
+            validator = new UserMoreInfoCreateViewModelValidator();
+            userMoreInfo = new UserMoreInfoCreateViewModel
+            {
+                UserId = new Guid("e618879f-966e-4bbb-b8d2-c497586e4405"),
+            };
+        }
+
+        [Test]
+        public void SetTitleForEvent_ValidTitle_ValidationErrorIsNotReturn()
+        {
+            userMoreInfo.UserId = new Guid("e618879f-966e-4bbb-b8d2-c497586e4446");
+
+            var result = validator.TestValidate(userMoreInfo);
+
+            // Assert
+            result.ShouldNotHaveValidationErrorFor(e => e.UserId);
+        }
+    }
+}
