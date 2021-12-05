339ff8d52197a24f6a8d07d6cd6ab26784fcae5b
r3d-scarf <pavel.shcherbatyi@protonmail.com>
2021-12-05 21:53:49 +0200

    Changed existing Rating component (#946)

--- EventsExpress/ClientApp/src/components/rating/rating-setter.js
+++ EventsExpress/ClientApp/src/components/rating/rating-setter.js
@@ -1,16 +1,18 @@
-import React from 'react'
-import Rating from '@material-ui/lab/Rating';
+import React from "react";
+import Rating from "react-rating";
 
-
-export default function RatingSetter(props) {    
-    
-    return <div>
-        Your rate: 
-        <Rating 
-            value={Number(props.myRate)}
-            max={10}
-            size="large"
-            onChange={props.callback}
-        />
+export default function RatingSetter(props) {
+  return (
+    <div>
+      Your rate:
+      <Rating
+        initialRating={Number(props.myRate)}
+        stop={10}
+        step={2}
+        emptySymbol="fa fa-star-o fa-2x medium"
+        fullSymbol="fa fa-star fa-2x medium"
+        onChange={props.callback}
+      />
     </div>
+  );
 }
--- EventsExpress/ClientApp/src/containers/rating.js
+++ EventsExpress/ClientApp/src/containers/rating.js
@@ -1,53 +1,59 @@
-import React, {Component} from 'react';
-import { connect } from 'react-redux';
-import { set_rating, get_currrent_rating, get_average_rating } from'../actions/rating-action'
-import RatingAverage from '../components/rating/rating-average'
-import RatingSetter from '../components/rating/rating-setter'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import {
+  set_rating,
+  get_currrent_rating,
+  get_average_rating,
+} from "../actions/rating-action";
+import RatingAverage from "../components/rating/rating-average";
+import RatingSetter from "../components/rating/rating-setter";
 
-class RatingWrapper extends Component{
-    componentDidMount = () => {
-        
-        this.props.getMyRate();
-        this.props.getAverageRate();
-        
-    }
+class RatingWrapper extends Component {
+  componentDidMount = () => {
+    this.props.getMyRate();
+    this.props.getAverageRate();
+  };
 
-    onRateChange = event => {
-        let rate = event.currentTarget.value;
-        this.props.setRate(rate).then(this.props.getAverageRate);
-    }
+  onRateChange = (value) => {
+    this.props.setRate(value).then(this.props.getAverageRate);
+  };
 
-    render() {        
-        return <div className='d-flex flex-row align-items-center justify-content-between'>
-                    {this.props.iWillVisitIt  
-                        ? <RatingSetter myRate={this.props.myRate} callback={this.onRateChange} /> 
-                        : <div></div>
-                    }
-                    
-                    <RatingAverage value={this.props.averageRate} />
-                    
-                                                       
-            </div>
-    }
+  render() {
+    return (
+      <div className="d-flex flex-row align-items-center justify-content-between">
+        {this.props.iWillVisitIt ? (
+          <RatingSetter
+            myRate={this.props.myRate}
+            callback={this.onRateChange}
+          />
+        ) : (
+          <div></div>
+        )}
+
+        <RatingAverage value={this.props.averageRate} />
+      </div>
+    );
+  }
 }
 
-
 const mapStateToProps = (state) => ({
-    myRate: state.event.myRate, 
-    averageRate: state.event.averageRate
+  myRate: state.event.myRate,
+  averageRate: state.event.averageRate,
 });
 
-
-const mapDispatchToProps = (dispatch, props) => { 
-    return {
-        setRate: value => dispatch(set_rating({
-                eventId: props.eventId, 
-                userId: props.userId,
-                rate: value
-        })),       
-        getMyRate: () => dispatch(get_currrent_rating(props.eventId)),
-        getAverageRate: () => dispatch(get_average_rating(props.eventId)),
-    } 
+const mapDispatchToProps = (dispatch, props) => {
+  return {
+    setRate: (value) =>
+      dispatch(
+        set_rating({
+          eventId: props.eventId,
+          userId: props.userId,
+          rate: value,
+        })
+      ),
+    getMyRate: () => dispatch(get_currrent_rating(props.eventId)),
+    getAverageRate: () => dispatch(get_average_rating(props.eventId)),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(RatingWrapper);
