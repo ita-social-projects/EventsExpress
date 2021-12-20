aa5fd877f8b5faab08cb85ce1de5e34e5d1e2ff3
Vasyl Semeniuk <70913988+idontwannawakeup@users.noreply.github.com>
2021-12-20 13:56:51 +0200

    951 - [Home Page/Footer] As a user I want to get access to the extended info from the specified menu-items in Footer (#985)
    * Implemented footer layout
    
    * Added circle
    
    * Changed footer and main layout styles, now footer at the bottom of the page
    
    * Changed link to Contact us page
    
    * Fixed css classes

--- EventsExpress/ClientApp/src/components/MainLayout/MainLayout.js
+++ EventsExpress/ClientApp/src/components/MainLayout/MainLayout.js
@@ -1,11 +1,16 @@
-import React from "react";
-import Header from "./../Header"
+import React from 'react';
+import Header from './../Header';
+import { Footer } from '../footer/footer';
+import './main-layout.css'
 
-const MainLayout = ({children}) => {
-    return(<>
-    <Header />
-    {children}
-    </>)
+const MainLayout = ({ children }) => {
+    return (
+        <div className="page-wrapper">
+            <Header />
+            {children}
+            <Footer />
+        </div>
+    );
 };
 
 export default MainLayout;
