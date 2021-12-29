3e898b39adea2b49b984e22244e06f4d9b286c45
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2021-12-29 14:17:11 +0200

    Removed unused actions

--- EventsExpress/ClientApp/src/actions/events/filter/organizer-filter.js
+++ EventsExpress/ClientApp/src/actions/events/filter/organizer-filter.js
@@ -1,22 +1,10 @@
 import { UserService } from '../../../services';
 
 export const SET_ORGANIZERS = 'SET_ORGANIZERS';
-export const SET_SELECTED_ORGANIZERS = 'SET_SELECTED_ORGANIZERS';
-export const DELETE_ORGANIZER_FROM_SELECTED = 'DELETE_ORGANIZER_FROM_SELECTED ';
 
 export const setOrganizers = organizers => ({
     type: SET_ORGANIZERS,
     payload: organizers
-});
-
-export const setSelectedOrganizers = organizers => ({
-    type: SET_SELECTED_ORGANIZERS,
-    payload: organizers
-});
-
-export const deleteOrganizerFromSelected = organizer => ({
-    type: DELETE_ORGANIZER_FROM_SELECTED,
-    payload: organizer
 });
 
 const userService = new UserService();
--- EventsExpress/ClientApp/src/reducers/events-filter/organizer-filter.js
+++ EventsExpress/ClientApp/src/reducers/events-filter/organizer-filter.js
@@ -1,12 +1,7 @@
-import {
-    SET_ORGANIZERS,
-    DELETE_ORGANIZER_FROM_SELECTED,
-    SET_SELECTED_ORGANIZERS
-} from '../../actions/events/filter/organizer-filter';
+import { SET_ORGANIZERS } from '../../actions/events/filter/organizer-filter';
 
 const initialState = {
-    organizers: [],
-    selectedOrganizers: []
+    organizers: []
 };
 
 export const organizerFilterReducer = (state = initialState, action) => {
@@ -15,17 +10,6 @@ export const organizerFilterReducer = (state = initialState, action) => {
             return {
                 ...state,
                 organizers: action.payload
-            };
-        case SET_SELECTED_ORGANIZERS:
-            return {
-                ...state,
-                selectedOrganizers: action.payload
-            };
-        case DELETE_ORGANIZER_FROM_SELECTED:
-            return {
-                ...state,
-                selectedOrganizers: state.selectedOrganizers.filter(
-                    organizer => organizer.id !== action.payload.id)
             };
         default:
             return state;
