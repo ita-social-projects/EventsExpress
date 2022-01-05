96391117d4f1fc9718790cd469932b943d387c39
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-01-05 19:10:59 +0200

    Refactored applyFilters action

--- EventsExpress/ClientApp/src/actions/events/filter/actions.js
+++ EventsExpress/ClientApp/src/actions/events/filter/actions.js
@@ -1,14 +1,18 @@
-import { stringify } from 'query-string';
+import { stringify, exclude } from 'query-string';
 import { get_events } from '../../event/event-list-action';
 
 export const applyFilters = filters => {
     return async dispatch => {
-        const filter = stringify({
-            ...filters,
-            organizers: undefined,
-            owners: filters?.organizers?.map(organizer => organizer.id)
-        }, { arrayFormat: 'index', skipNull: true });
-        dispatch(get_events(`?${filter}`));
+        filters.owners = filters?.organizers?.map(organizer => organizer.id);
+
+        const options = { arrayFormat: 'index', skipNull: true };
+        const filter = exclude(
+            `?${stringify(filters, options)}`,
+            ['organizers'],
+            options
+        );
+
+        dispatch(get_events(filter));
     };
 };
 
