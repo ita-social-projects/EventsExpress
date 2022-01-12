818d6a306ab2e47bd6e49c4b6763e68f8218c0a1
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-01-12 21:55:48 +0200

    Changed delay hook

--- EventsExpress/ClientApp/src/components/event/filter/parts/organizer/delay-hook.js
+++ EventsExpress/ClientApp/src/components/event/filter/parts/organizer/delay-hook.js
@@ -1,9 +1,15 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
-export const useDelay = (delayedAction, initialValue, timeout = 1000) => {
+export const useDelay = (delayedAction, initialValue, skipFirstRun = true, timeout = 1000) => {
     const [value, setValue] = useState(initialValue);
 
+    const isFirstRun = useRef(skipFirstRun);
     useEffect(() => {
+        if (isFirstRun.current) {
+            isFirstRun.current = false;
+            return;
+        }
+
         const timeoutId = setTimeout(() => {
             delayedAction(value);
         }, timeout);
