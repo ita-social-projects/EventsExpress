b6d661b87f31b568d2cdc35716f9fb66b35514e2
rusnakvlad <vlad.rusnak2@gmail.com>
2022-01-12 15:48:24 +0200

    overridedValidationPropName

--- EventsExpress/Validation/EditUserNameViewModelValidator.cs
+++ EventsExpress/Validation/EditUserNameViewModelValidator.cs
@@ -8,11 +8,11 @@ namespace EventsExpress.Validation
     {
         public EditUserNameViewModelValidator()
         {
-            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
-            RuleFor(x => x.Name).Must(x => char.IsLetter(x.First())).WithMessage("Username must begin with a letter");
-            RuleFor(x => x.Name).Must(x => char.IsLetter(x.Last())).WithMessage("Username cannot contain special characters at the end");
-            RuleFor(x => x.Name).Must(x => !x.Contains(' ')).WithMessage("Spaces are not allowed");
-            RuleFor(x => x.Name).Must(x => x.Length >= 3 && x.Length <= 25).WithMessage("Username should be between 3 and 25 characters in length");
+            RuleFor(x => x.Name).NotEmpty().OverridePropertyName("userName").WithMessage("Name is required");
+            RuleFor(x => x.Name).Must(x => char.IsLetter(x.First())).OverridePropertyName("userName").WithMessage("Username must begin with a letter");
+            RuleFor(x => x.Name).Must(x => char.IsLetter(x.Last())).OverridePropertyName("userName").WithMessage("Username cannot contain special characters at the end");
+            RuleFor(x => x.Name).Must(x => !x.Contains(' ')).OverridePropertyName("userName").WithMessage("Spaces are not allowed");
+            RuleFor(x => x.Name).Must(x => x.Length >= 3 && x.Length <= 25).OverridePropertyName("userName").WithMessage("Username should be between 3 and 25 characters in length");
         }
     }
 }
