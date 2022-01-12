6e0e492c4620b7d0eddc82fd2a1bb179c83ddf89
Andrew <andrewmakoviichuk@gmail.com>
2022-01-12 17:41:38 +0200

    Removed redundant value from props

--- EventsExpress/ClientApp/src/components/event/event-item.js
+++ EventsExpress/ClientApp/src/components/event/event-item.js
@@ -87,7 +87,6 @@ export default class EventCard extends Component {
                         owners={owners}
                         dateFrom={dateFrom}
                         title={title}
-                        onlyAdults={isOnlyForAdults}
                     />
                     <CardMedia
                         className={classes.media}
