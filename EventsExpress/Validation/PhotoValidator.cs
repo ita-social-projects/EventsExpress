b4757dd830311e0d6af9665e6427466aa6393b9a
rusnakvlad <vlad.rusnak2@gmail.com>
2022-01-13 16:53:45 +0200

    addedValidationRulesForAvatar

--- EventsExpress/Validation/PhotoValidator.cs
+++ EventsExpress/Validation/PhotoValidator.cs
@@ -1,17 +1,34 @@
 ﻿namespace EventsExpress.Validation
 {
+    using System.IO;
+    using System.Linq;
     using EventsExpress.Core.Extensions;
     using EventsExpress.ViewModels;
     using FluentValidation;
+    using FreeImageAPI;
     using Microsoft.AspNetCore.Http;
 
     public class PhotoValidator : AbstractValidator<PhotoViewModel>
     {
+        private readonly string[] allowedExtensions = { "jpeg", "jpg", "png", "bmp" };
+        private readonly double tenMB = 10485760;
+
         public PhotoValidator()
         {
             RuleFor(f => f.Photo).NotEmpty().Must(ValidImage).WithMessage("The upload file should be a valid image!");
+            RuleFor(f => f.Photo).Must(f => f.Length > tenMB).WithMessage("File size can not exceed 10 MB");
+            RuleFor(f => f.Photo).Must(f => allowedExtensions.Contains(Path.GetExtension(f.FileName))).
+                WithMessage("Accepted file formats are .jpeg, .jpg, .png, or .bmp");
+            RuleFor(f => f.Photo).Must(ValidImageSize).WithMessage("Image size should be at least 750x750px");
         }
 
         private bool ValidImage(IFormFile file) => file.IsImage();
+
+        private bool ValidImageSize(IFormFile file)
+        {
+            using var memoryStream = file.ToMemoryStream();
+            var image = new FreeImageBitmap(memoryStream);
+            return image.Width >= 750 && image.Height >= 750;
+        }
     }
 }
