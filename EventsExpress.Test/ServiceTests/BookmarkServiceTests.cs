1f619d2ce33bf3a2dd334b612234db2d450b035d
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-02-03 12:08:39 +0200

    Added tests for BookmarkService

---
+++ EventsExpress.Test/ServiceTests/BookmarkServiceTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using EventsExpress.Core.Services;
+using EventsExpress.Db.Bridge;
+using EventsExpress.Db.Entities;
+using EventsExpress.Test.ServiceTests.TestClasses.Bookmark;
+using EventsExpress.Test.ServiceTests.TestClasses.Event;
+using Moq;
+using NUnit.Framework;
+
+namespace EventsExpress.Test.ServiceTests;
+
+[TestFixture]
+public class BookmarkServiceTests : TestInitializer
+{
+    private BookmarkService _service;
+    private Mock<ISecurityContext> _securityContext;
+
+    [SetUp]
+    protected override void Initialize()
+    {
+        base.Initialize();
+
+        Context.Users.AddRange(EventTestData.Users);
+        Context.Events.AddRange(EventTestData.Events);
+        Context.EventBookmarks.Add(new EventBookmark
+        {
+            UserFromId = EventTestData.FirstUserId,
+            EventId = EventTestData.ThirdEventId,
+        });
+        Context.SaveChanges();
+
+        _securityContext = new Mock<ISecurityContext>();
+        _service = new BookmarkService(Context, _securityContext.Object);
+    }
+
+    [TestCaseSource(typeof(NotBookmarkedEventsIds))]
+    public async Task SaveEventToBookmarksAsync_WhenValidIdsPassed_ShouldSaveBookmark(
+        Guid userId,
+        Guid eventId)
+    {
+        _securityContext.Setup(c => c.GetCurrentUserId()).Returns(userId);
+        var expected = Context.EventBookmarks.Count() + 1;
+
+        await _service.SaveEventToBookmarksAsync(eventId);
+        var actual = Context.EventBookmarks.Count();
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestCaseSource(typeof(BookmarkedEventsIds))]
+    public async Task DeleteEventFromBookmarksAsync_WhenBookmarkExists_ShouldDeleteBookmark(
+        Guid userId,
+        Guid eventId)
+    {
+        _securityContext.Setup(c => c.GetCurrentUserId()).Returns(userId);
+        var expected = Context.EventBookmarks.Count() - 1;
+
+        await _service.DeleteEventFromBookmarksAsync(eventId);
+        var actual = Context.EventBookmarks.Count();
+
+        Assert.AreEqual(expected, actual);
+    }
+}
---
+++ EventsExpress.Test/ServiceTests/TestClasses/Bookmark/BookmarkedEventsIds.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using EventsExpress.Test.ServiceTests.TestClasses.Event;
+using NUnit.Framework;
+
+namespace EventsExpress.Test.ServiceTests.TestClasses.Bookmark;
+
+public class BookmarkedEventsIds : IEnumerable
+{
+    public IEnumerator GetEnumerator()
+    {
+        yield return new TestCaseData(EventTestData.FirstUserId, EventTestData.ThirdEventId);
+    }
+}
