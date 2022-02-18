dc9f64458a2bae249ded4db41cce23e4c8a6f3e2
Yaroslav <slavikmelnyk222@gmail.com>
2022-02-18 02:47:35 +0200

    Created Terms Page

--- EventsExpress/ClientApp/src/components/app/app.js
+++ EventsExpress/ClientApp/src/components/app/app.js
@@ -37,6 +37,7 @@ import AuthUser from '../../actions/login/auth-user-action';
 import getConfig from '../../actions/config/get-config-action';
 import RegistrationForm from '../RegistrationForm';
 import MainLayout from '../MainLayout';
+import Terms from '../terms/terms.js';
 
 class App extends Component {
     constructor(props) {
@@ -94,6 +95,7 @@ class App extends Component {
                                 <Route path='/drafts' component={this.UserRoleSecurity(EventDraftListWrapper)} />
                                 <Route path='/unauthorized' component={Unauthorized} />
                                 <Route path='/forbidden' component={Forbidden} />
+                                <Route path='/terms' component={Terms}/>
                                 <Route component={NotFound} />
                             </Switch>
                         </Layout>
