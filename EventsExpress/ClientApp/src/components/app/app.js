f6fe4be969d2e45a4a188e0f9787aa7d3d1e6e8b
Bohdan Bandura <bodia0b@gmail.com>
2022-02-20 23:49:44 +0200

    Created About page

--- EventsExpress/ClientApp/src/components/app/app.js
+++ EventsExpress/ClientApp/src/components/app/app.js
@@ -37,6 +37,7 @@ import AuthUser from '../../actions/login/auth-user-action';
 import getConfig from '../../actions/config/get-config-action';
 import RegistrationForm from '../RegistrationForm';
 import MainLayout from '../MainLayout';
+import About from "../about/about.js"
 
 class App extends Component {
     constructor(props) {
@@ -92,6 +93,7 @@ class App extends Component {
                                 <Route path='/registerSuccess' component={RegisterSuccess} />
                                 <Route path='/editEvent/:id' component={this.UserRoleSecurity(EventEditWrapper)} />
                                 <Route path='/drafts' component={this.UserRoleSecurity(EventDraftListWrapper)} />
+                                <Route path='/about' component={About} />
                                 <Route path='/unauthorized' component={Unauthorized} />
                                 <Route path='/forbidden' component={Forbidden} />
                                 <Route component={NotFound} />
