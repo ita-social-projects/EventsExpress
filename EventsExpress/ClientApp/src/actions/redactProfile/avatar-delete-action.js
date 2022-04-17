58b6b0603dd44526ad359608d97fd95fc73f4503
Bohdan Bandura <bodia0b@gmail.com>
2022-04-17 23:19:51 +0300

    - Removed extra code

--- EventsExpress/ClientApp/src/actions/redactProfile/avatar-delete-action.js
+++ EventsExpress/ClientApp/src/actions/redactProfile/avatar-delete-action.js
@@ -4,12 +4,6 @@ import { SubmissionError } from 'redux-form';
 import { buildValidationState } from '../../components/helpers/action-helpers.js'
 import { getRequestInc, getRequestDec } from "../request-count-action";
 
-export const deleteAvatar = {
-    PENDING: "SET_DELETE_AVATAR_PENDING",
-    SUCCESS: "SET_DELETE_AVATAR_SUCCESS",
-    UPDATE: "UPDATE_DELETE_AVATAR"
-}
-
 const api_serv = new PhotoService();
 
 export default function delete_avatar(data) {
@@ -21,17 +15,10 @@ export default function delete_avatar(data) {
             throw new SubmissionError(await buildValidationState(response));
         }
         dispatch(getRequestDec());
-        dispatch(updateAvatar());
         dispatch(setSuccessAllert('Avatar is successfully deleted'));
         return Promise.resolve();
     }
 }
 
 
-
-export function updateAvatar() {
-    return {
-        type: deleteAvatar.UPDATE,
-    };
-}
 
--- EventsExpress/ClientApp/src/components/profile/profile-sections/profile-avatar.js
+++ EventsExpress/ClientApp/src/components/profile/profile-sections/profile-avatar.js
@@ -1,35 +1,30 @@
-import React from 'react'
-import CustomAvatar from '../../avatar/custom-avatar'
-import IconButton from '@material-ui/core/IconButton';
-import { makeStyles } from '@material-ui/core/styles';
-import EditAvatarModal from '../editProfile/editAvatarModals/edit-avatar-modal-with-options'
-
+import React from "react";
+import CustomAvatar from "../../avatar/custom-avatar";
+import IconButton from "@material-ui/core/IconButton";
+import { makeStyles } from "@material-ui/core/styles";
+import EditAvatarModal from "../editProfile/editAvatarModals/edit-avatar-modal-with-options";
 
 const useStyles = makeStyles({
   editIcon: {
-    position:'absolute',
-    display:'block',
-    left:"50%" ,
+    position: "absolute",
+    display: "block",
+    left: "50%",
     top: "80%",
     transform: "translateX(-50%)",
-    zIndex:10,
-    textAlign:"center",
-    backgroundColor:"#B8B8B8",
-    '&:active svg':{
-      color :"red"
-    }
-
-},
-avatar:{
-  position:'relative',
-  backgroundClip: "content-box",
-   '&:hover $editIcon':{
-     backgroundColor:"#B8B8B8"
-     
-   },
-},
-
-
+    zIndex: 10,
+    textAlign: "center",
+    backgroundColor: "#B8B8B8",
+    "&:active svg": {
+      color: "red",
+    },
+  },
+  avatar: {
+    position: "relative",
+    backgroundClip: "content-box",
+    "&:hover $editIcon": {
+      backgroundColor: "#B8B8B8",
+    },
+  },
 });
 
 export default function ProfileAvatar(props) {
@@ -37,25 +32,34 @@ export default function ProfileAvatar(props) {
   const [modalShow, setModalShow] = React.useState(false);
   const handleOpen = () => {
     setModalShow(true);
-  }
-  const handleClose = () =>{
+  };
+  const handleClose = () => {
     setModalShow(false);
-  }
+  };
   return (
     <div className={classes.avatar}>
-            <IconButton aria-label="delete" className={classes.editIcon} onClick={() => setModalShow(true)} >
-              <i className="fas fa-camera"></i>
-           </IconButton>
+      <IconButton
+        aria-label="delete"
+        className={classes.editIcon}
+        onClick={() => setModalShow(true)}
+      >
+        <i className="fas fa-camera"></i>
+      </IconButton>
 
-        <CustomAvatar height= "300px" width=  "300px"  name={props.name} userId={props.userId}/>
+      <CustomAvatar
+        height="300px"
+        width="300px"
+        name={props.name}
+        userId={props.userId}
+      />
 
-        <EditAvatarModal
-          show={modalShow}
-          onHide={handleClose}
-          Open={handleOpen}
-          name ={props.name}
-          id = {props.userId}
-        />
+      <EditAvatarModal
+        show={modalShow}
+        onHide={handleClose}
+        Open={handleOpen}
+        name={props.name}
+        id={props.userId}
+      />
     </div>
-)
+  );
 }
