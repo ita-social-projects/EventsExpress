c8a5a8ec5163a837265d0104b852bfb67de8d894
Maks <lion20914king@gmail.com>
2022-05-20 12:23:31 +0300

    Add migration with deleted UserName

---
+++ EventsExpress.Db/Migrations/20220520085331_RemoveUserName.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventsExpress.Db.Migrations
+{
+    public partial class RemoveUserName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Users");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
